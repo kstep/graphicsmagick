80662bd47c8c4efe37d711301f7dd65cb66b30bd
John Cristy <duc.sequere.aut.de.via.decede@gmail.com>
1999-08-14 13:09:25 -0500

    *** empty log message ***

--- magick/montage.c
+++ magick/montage.c
@@ -10,7 +10,7 @@
 %              M   M   OOO   N   N    T    A   A   GGG   EEEEE                %
 %                                                                             %
 %                                                                             %
-%                     ImageMagick Image Montage Methods                       %
+%              ImageMagick Methods to Create Image Thumbnails                 %
 %                                                                             %
 %                                                                             %
 %                              Software Design                                %
