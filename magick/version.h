695dd5411b227b0fb414c3b4b84cff2fbe47c553
John Cristy <duc.sequere.aut.de.via.decede@gmail.com>
1999-12-01 23:21:23 -0600

    *** empty log message ***

--- magick/version.h
+++ magick/version.h
@@ -15,8 +15,13 @@ extern "C" {
   "\n           portions copyright Eastman Kodak Company, 1996"
 #endif
 #define MagickLibVersion  0x0510
+#if defined(QuantumLeap)
 #define MagickVersion  \
-  "@(#)ImageMagick 5.1.0 00/01/01 cristy@mystic.es.dupont.com"
+  "@(#)ImageMagick 5.1.0 00/01/01 Q:16 cristy@mystic.es.dupont.com"
+#else
+#define MagickVersion  \
+  "@(#)ImageMagick 5.1.0 00/01/01 Q:8 cristy@mystic.es.dupont.com"
+#endif
 
 #if defined(__cplusplus) || defined(c_plusplus)
 }
