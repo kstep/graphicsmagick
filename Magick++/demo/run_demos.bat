5e9862e2e34102f0f1a6164aff2cf82e777d3c1f
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
1999-12-28 14:23:08 -0600

    Moved to VisualMagick directory.

--- Magick++/demo/run_demos.bat
+++
@@ -1,9 +0,0 @@
-: Batch script to run Magick++ demos
-@echo !!! button !!!
-@..\..\VisualMagick\bin\button
-@echo !!! demo !!!
-@..\..\VisualMagick\bin\demo
-@echo !!! flip !!!
-@..\..\VisualMagick\bin\flip
-@echo !!! shapes !!!
-@..\..\VisualMagick\bin\shapes
