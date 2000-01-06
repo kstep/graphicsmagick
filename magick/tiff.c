32da6e1353e9d545cad6e92b29492903f7b842ab
John Cristy <duc.sequere.aut.de.via.decede@gmail.com>
2000-01-06 16:03:56 -0600

    *** empty log message ***

--- magick/tiff.c
+++ magick/tiff.c
@@ -1127,12 +1127,9 @@ Export unsigned int WriteTIFFImage(const ImageInfo *image_info,Image *image)
         next_image=ZoomImage(image,width,height);
         if (next_image == (Image *) NULL)
           WriterExit(FileOpenWarning,"Unable to pyramid encode image",image);
-        next_image->exempt=True;
+        next_image->next=(Image *) NULL;
         if (pyramid_image == (Image *) NULL)
-          {
-            pyramid_image=next_image;
-            pyramid_image->next=(Image *) NULL;
-          }
+          pyramid_image=next_image;
         else
           {
             register Image
