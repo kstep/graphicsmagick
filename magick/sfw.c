4ea376605025a2848cabb7947cfd99ebb5568899
John Cristy <duc.sequere.aut.de.via.decede@gmail.com>
2000-01-07 16:58:40 -0600

    *** empty log message ***

--- magick/sfw.c
+++ magick/sfw.c
@@ -153,6 +153,7 @@ static void TranslateSFWMarker(unsigned char *marker)
     case 0xd0: marker[1]=0xe0; break;  /* app */
     case 0xcb: marker[1]=0xdb; break;  /* dqt */
     case 0xa0: marker[1]=0xc0; break;  /* sof */
+    case 0xa4: marker[1]=0xc4; break;  /* sof */
     case 0xca: marker[1]=0xda; break;  /* sos */
     case 0xc9: marker[1]=0xd9; break;  /* eoi */
     default: break;
