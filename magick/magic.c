3a68013cc6f331103cd99b1c3ea861e019baba3a
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2000-03-25 11:02:39 -0600

    Include magic.mgk in dist.

--- magick/magic.c
+++ magick/magic.c
@@ -59,4 +59,52 @@
   Global declarations.
 */
 
+/* Available magic matching methods */
+typedef enum
+{
+  UndefinedMagicMethod,
+  StringMagicMethod
+} MagicMethod;
+
+/* Argument to support StringMagickMethod */
+#define StringMethodArgumentExtent 32
+typedef struct _StringMethodArgument
+{
+  unsigned int
+    value_length,
+    value_offset;
+
+  unsigned char
+    value[StringMethodArgumentExtent];
+
+} StringMethodArgument;
+
+/* List member to support one or more tests for a format */
+typedef struct _MagicListMember
+{
+  MagicMethod
+    method;      /* Method to apply */
+
+  void
+    *argument;   /* Method argument, e.g. cast to StringMethodArgument* */
+
+  int
+    truth_value; /* Truth value of operation (True or False) */
+
+  struct _MagicListMember
+    *next;
+
+} MagicListMember;
+
+/* List of formats to test */
+typedef struct _MagicList
+{
+  char
+    *tag;
+
+  struct _MagicListMember
+    *member;
+
+} MagicList;
+
 
