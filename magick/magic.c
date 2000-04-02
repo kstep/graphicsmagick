90d40659b019f0be1182f202f55b232acd09dc7f
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2000-04-02 12:51:08 -0500

    Parser parses correctly now.  Still does not build data structure.

--- magick/magic.c
+++ magick/magic.c
@@ -82,6 +82,8 @@ Export unsigned int GetImageMagic(char* magic,
   if(magic_test_list == (MagicTest**) NULL)
     IntializeImageMagic();
 
+exit(0);
+
   /* Traverse magic tests */
   for (i=0;magic_test_list[i]!=(MagicTest*)NULL;++i)
     {
@@ -180,8 +182,8 @@ static void IntializeImageMagic(void)
     test_member;
 
   strcpy(file_name,DelegatePath);
-  strcpy(file_name,DirectorySeparator);
-  strcpy(file_name,"magic.mgk");
+  strcat(file_name,DirectorySeparator);
+  strcat(file_name,"magic.mgk");
 
   file = fopen(file_name, "r");
   if(file != (FILE*) NULL)
@@ -202,25 +204,18 @@ static void IntializeImageMagic(void)
           while(isalnum((int)*buff_p))
             *tag_p++=*buff_p++;
           *tag_p='\0';
+          printf("TAG:\"%s\"\n", tag);
           if(*buff_p == '\0')
-            {
-              printf("%s:%d: syntax: \"%s\"\n", file_name, line_number,
-                     "unexpected end of line");
-              continue;
-            }
+            goto eol_error;
 
           /* parse sequence of match rules */
           while(1)
             {
               /* skip over white space */
-              while(isspace((int)*buff_p))
+              while(isspace((int)*(buff_p)))
                 ++buff_p;
               if(*buff_p == '\0')
-                {
-                  printf("%s:%d: syntax: \"%s\"\n", file_name, line_number,
-                         "unexpected end of line");
-                  continue;
-                }
+                goto eol_error;
 
               /* intialize test_member */
               test_member.method=UndefinedMagicMethod;
@@ -229,27 +224,26 @@ static void IntializeImageMagic(void)
               test_member.next=(MagicTestMember*)NULL;
 
               /* test truth value */
-              if(*buff_p++ == '!')
-                test_member.truth_value=False;
-
-              /* skip over white space */
-              while(isspace((int)*buff_p))
-                ++buff_p;
-              if(*buff_p == '\0')
+              if(*buff_p == '!')
                 {
-                  printf("%s:%d: syntax: \"%s\"\n", file_name, line_number,
-                         "unexpected end of line");
-                  break;
+                  ++buff_p;
+                  test_member.truth_value=False;
                 }
 
-              if(LocaleCompare(buff_p,"string(") == 0)
+              /* skip over white space */
+              while(isspace((int)*(buff_p)))
+                ++buff_p;
+              if(*buff_p == '\0')
+                goto eol_error;
+
+              if(LocaleNCompare(buff_p,"string(",7) == 0)
                 {
                   StringMethodArgument
                     string_argument;
 
                   unsigned char
                     *str_p;
-
+                  printf("Parsing string command\n");
                   test_member.method=StringMagicMethod;
                   string_argument.value_length=0;
                   string_argument.value_offset=0;
@@ -259,131 +253,99 @@ static void IntializeImageMagic(void)
                   buff_p += 7;
 
                   /* skip over white space */
-                  while(isspace((int)*buff_p))
+                  while(isspace((int)*(buff_p)))
                     ++buff_p;
                   if(*buff_p == '\0')
-                    {
-                      printf("%s:%d: syntax: \"%s\"\n", file_name, line_number,
-                             "unexpected end of line");
-                      break;
-                    }
+                    goto eol_error;
 
                   /* get offset */
                   string_argument.value_offset=strtol(buff_p, &buff_p, 10);
+                  printf("Offset=%u\n", string_argument.value_offset);
 
                   /* skip over white space */
-                  while(isspace((int)*buff_p))
+                  while(isspace((int)*(buff_p)))
                     ++buff_p;
                   if(*buff_p == '\0')
-                    {
-                      printf("%s:%d: syntax: \"%s\"\n", file_name, line_number,
-                             "unexpected end of line");
-                      break;
-                    }
+                    goto eol_error;
 
                   /* check for comma */
                   if(*buff_p != ',')
-                    {
-                      printf("%s:%d: syntax: \"%s\"\n", file_name, line_number,
-                             buff_p);
-                      break;
-                    }
+                    goto syntax_error;
                   ++buff_p;
 
                   /* skip over white space */
-                  while(isspace((int)*buff_p))
+                  while(isspace((int)*(buff_p)))
                     ++buff_p;
                   if(*buff_p == '\0')
-                    {
-                      printf("%s:%d: syntax: \"%s\"\n", file_name, line_number,
-                             "unexpected end of line");
-                      break;
-                    }
+                    goto eol_error;
 
                   /* check for double quotes */
                   if(*buff_p != '"')
-                    {
-                      printf("%s:%d: syntax: \"%s\"\n", file_name, line_number,
-                             buff_p);
-                      break;
-                    }
+                    goto syntax_error;
                   ++buff_p;
 
                   /* translate string */
                   str_p=string_argument.value;
                   while(1)
                     {
-
                       /* unexpected end of line */
                       if(*buff_p == '\0')
+                        goto eol_error;
+
+                      /* end of string, done */
+                      if(*buff_p == '"')
                         {
-                          printf("%s:%d: syntax: \"%s\"\n", file_name,
-                                 line_number,
-                                 "unexpected end of line");
+                          *str_p='\0';
+                          ++buff_p;
                           break;
                         }
 
-                      /* end of string */
-                      if(*buff_p == '"')
-                        break;
+                      if(*buff_p == '\\')
+                        {
+                          ++buff_p;
+                          if(isdigit((int)*(buff_p)) &&
+                             isdigit((int)*(buff_p+1)) &&
+                             isdigit((int)*(buff_p+2)))
+                            {
+                              /* octal code */
+                              char
+                                lbuff[4];
 
-                      if(*buff_p == '\\');
-                      {
-                        ++buff_p;
-                        if(isdigit((int)*(buff_p)) &&
-                           isdigit((int)*(buff_p+1)) &&
-                           isdigit((int)*(buff_p+2)))
-                          {
-                            /* octal code */
-                            char
-                              lbuff[4];
+                              lbuff[0]=*buff_p++;
+                              lbuff[1]=*buff_p++;
+                              lbuff[2]=*buff_p++;
+                              lbuff[3]='\0';
 
-                            lbuff[0]=*buff_p++;
-                            lbuff[1]=*buff_p++;
-                            lbuff[2]=*buff_p++;
-                            lbuff[3]='\0';
-
-                            *str_p++=(unsigned char)strtol(lbuff,
+                              *str_p=(unsigned char)strtol(lbuff,
                                                            (char**)NULL,8);
-                            ++string_argument.value_length;
-                            buff_p += 3;
-                          }
-                        else
-                          {
-                            /* escaped character */
-                            *str_p++=*buff_p++;
-                            ++string_argument.value_length;
-                          }
-                        continue;
-                      }
+                              ++str_p;
+                              ++string_argument.value_length;
+                              continue;
+                            }
+                        }
 
-                      /* any other character */
-                      *str_p++=*buff_p++;
+                      /* copy character character */
+                      *str_p=*buff_p;
+                      ++str_p;
+                      ++buff_p;
                       ++string_argument.value_length;
-
                     }
+                  printf("Length=%u\n", string_argument.value_length);
+                  printf("Value=\"%s\"\n", string_argument.value);
 
                   /* skip over white space */
-                  while(isspace((int)*buff_p))
+                  while(isspace((int)*(buff_p)))
                     ++buff_p;
                   if(*buff_p == '\0')
-                    {
-                      printf("%s:%d: syntax: \"%s\"\n", file_name,
-                             line_number, "unexpected end of line");
-                      break;
-                    }
+                    goto eol_error;
 
                   /* test parens */
                   if(*buff_p != ')')
-                    {
-                      printf("%s:%d: syntax: \"%s\"\n", file_name,
-                             line_number, buff_p);
-                      break;
-                    }
+                    goto syntax_error;
                   ++buff_p;
 
                   /* skip over white space */
-                  while(isspace((int)*buff_p))
+                  while(isspace((int)*(buff_p)))
                     ++buff_p;
 
                   if(*buff_p == ';')
@@ -397,13 +359,19 @@ static void IntializeImageMagic(void)
                     break;
                 }
               else
-                {
-                  printf("%s:%d: syntax: \"%s\"\n", file_name, line_number,
-                         buff_p);
-                  continue;
-                }
-
+                goto syntax_error;
             }
+          continue;
+          
+          syntax_error :
+            printf("%s:%d: syntax: \"%s\"\n", file_name,
+                   line_number, buff_p);
+          continue;
+          
+          eol_error :
+            printf("%s:%d: syntax: \"%s\"\n", file_name,
+                   line_number, "unexpected end of line");
+          continue;
         }
       fclose(file);
     }
