6507a24d9aa17004ea90315aeb05bea5c7abcde4
John Cristy <duc.sequere.aut.de.via.decede@gmail.com>
2000-07-08 22:02:43 -0500

    *** empty log message ***

--- magick/enhance.c
+++ magick/enhance.c
@@ -513,9 +513,9 @@ MagickExport unsigned int ModulateImage(Image *image,const char *modulate)
   assert(image->signature == MagickSignature);
   if (modulate == (char *) NULL)
     return(False);
-  percent_hue=0.0;
-  percent_brightness=0.0;
-  percent_saturation=0.0;
+  percent_hue=100.0;
+  percent_brightness=100.0;
+  percent_saturation=100.0;
   (void) sscanf(modulate,"%lf,%lf,%lf",&percent_brightness,&percent_saturation,
     &percent_hue);
   (void) sscanf(modulate,"%lf/%lf/%lf",&percent_brightness,&percent_saturation,
