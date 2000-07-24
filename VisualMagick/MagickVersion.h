a054bd40fef50bd58d16ee59e54c1cf50dfb0130
Bill Radcliffe <bill.radcliffe@gmail.com>
2000-07-24 14:12:38 -0500

    Updated to new version number.

--- VisualMagick/MagickVersion.h
+++ VisualMagick/MagickVersion.h
@@ -1,7 +1,7 @@
 // version.h  -  Version information for the ImageMagick product (used with the MS compiler).
 
-#define THIS_PRODUCT_VERSION_NUM		5,2,0,0
-#define THIS_PRODUCT_VERSION_STRING		"5.2.0"
+#define THIS_PRODUCT_VERSION_NUM		5,2,2,0
+#define THIS_PRODUCT_VERSION_STRING		"5.2.2"
 #define THIS_PRODUCT_NAME			"ImageMagick"
 
 #define THIS_PROJECT_DEBUG_EXT			"DB"
