89e84332ecbf98180dee7ff388dadcde1f6d87a1
John Cristy <duc.sequere.aut.de.via.decede@gmail.com>
2000-08-20 14:00:46 -0500

    *** empty log message ***

--- PerlMagick/Magick.pm
+++ PerlMagick/Magick.pm
@@ -26,7 +26,7 @@ require AutoLoader;
       CacheError StreamError Success Transparent Opaque
     );
 
-$VERSION = '5.22';
+$VERSION = '5.23';
 
 sub AUTOLOAD {
     # This AUTOLOAD is used to 'autoload' constants from the constant()
