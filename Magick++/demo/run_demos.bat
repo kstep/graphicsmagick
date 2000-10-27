c626eb4bd6de0b2040119787cebf2c618393837d
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2000-10-26 22:16:24 -0500

    File no longer needed

--- Magick++/demo/run_demos.bat
+++
@@ -1,8 +0,0 @@
-: Batch script to run Magick++ demos
-@echo !!! button !!!
-@..\..\VisualMagick\bin\button
-@echo !!! demo !!!
-@..\..\VisualMagick\bin\demo
-@echo !!! flip !!!
-@..\..\VisualMagick\bin\flip
-@echo !!! shapes !!!
