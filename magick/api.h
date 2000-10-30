9107e5a38274658105b5da8b16e9953bd7db9868
Bill Radcliffe <bill.radcliffe@gmail.com>
2000-10-29 18:23:31 -0600

    Minor change to add a pragma that makes Magick++ compile cleaner.

--- magick/api.h
+++ magick/api.h
@@ -51,6 +51,7 @@
 #pragma warning(disable : 4018)
 #pragma warning(disable : 4244)
 #pragma warning(disable : 4142)
+#pragma warning(disable : 4800)
 
 #else
 # define MagickExport __declspec(dllexport)
