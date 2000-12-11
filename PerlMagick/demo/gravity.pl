bf43a014579355ec78c028003c0b9c675723a348
John Cristy <duc.sequere.aut.de.via.decede@gmail.com>
2000-12-10 18:17:08 -0600

    *** empty log message ***

--- PerlMagick/demo/gravity.pl
+++ PerlMagick/demo/gravity.pl
@@ -2,9 +2,9 @@
 
 use Image::Magick;
 
-$font = '-adobe-helvetica-medium-r-normal--25-180-100-100-p-130-iso8729-1';
-$font = 'Arial';
-$font = 'Arial.ttf';
+#$font = '-adobe-helvetica-medium-r-normal--25-180-100-100-p-130-iso8729-1';
+#$font = 'Times';
+$font = 'Generic.ttf';
 
 $image = Image::Magick->new();
 $x = 100;
@@ -12,29 +12,29 @@ $y = 100;
 for ($angle=0; $angle < 360; $angle+=30)
 {
   print "angle $angle\n";
-  $label = Image::Magick->new(size=>"600x600",density=>72,pointsize=>30,
-    font=>$font,fill=>'black');
+  $label=Image::Magick->new(size=>"600x600",pointsize=>24,font=>$font,
+    fill=>'black');
   $label->Read("xc:white");
   $label->Draw(primitive=>'line',points=>"300,100 300,500",stroke=>'#600');
   $label->Draw(primitive=>'line',points=>"100,300 500,300",stroke=>'#600');
   $label->Draw(primitive=>'rectangle',points=>"100,100 500,500",fill=>'none',
     stroke=>'#600');
-  $label->Annotate(text=>"Cyborg",gravity=>"NorthWest",x=>$x,y=>$y,
+  $label->Annotate(text=>"North West",gravity=>"NorthWest",x=>$x,y=>$y,
     rotate=>$angle);
-  $label->Annotate(text=>"Cyborg",gravity=>"North",x=>$x,y=>$y,
+  $label->Annotate(text=>"North",gravity=>"North",x=>$x,y=>$y,
     rotate=>$angle);
-  $label->Annotate(text=>"Cyborg",gravity=>"NorthEast",x=>$x,y=>$y,
+  $label->Annotate(text=>"North East",gravity=>"NorthEast",x=>$x,y=>$y,
     rotate=>$angle);
-  $label->Annotate(text=>"Cyborg",gravity=>"East",x=>$x,y=>$y,
+  $label->Annotate(text=>"East",gravity=>"East",x=>$x,y=>$y,
     rotate=>$angle);
-  $label->Annotate(text=>"Cyborg",gravity=>"Center",x=>$x,y=>$y,
+  $label->Annotate(text=>"Center",gravity=>"Center",x=>$x,y=>$y,
     rotate=>$angle);
-  $label->Annotate(text=>"Cyborg",gravity=>"SouthEast",x=>$x,y=>$y,
+  $label->Annotate(text=>"South East",gravity=>"SouthEast",x=>$x,y=>$y,
     rotate=>$angle);
-  $label->Annotate(text=>"Cyborg",gravity=>"South",x=>$x,y=>$y,rotate=>$angle);
-  $label->Annotate(text=>"Cyborg",gravity=>"SouthWest",x=>$x,y=>$y,
+  $label->Annotate(text=>"South",gravity=>"South",x=>$x,y=>$y,rotate=>$angle);
+  $label->Annotate(text=>"South West",gravity=>"SouthWest",x=>$x,y=>$y,
     rotate=>$angle);
-  $label->Annotate(text=>"Cyborg",gravity=>"West",x=>$x,y=>$y,rotate=>$angle);
+  $label->Annotate(text=>"West",gravity=>"West",x=>$x,y=>$y,rotate=>$angle);
   push(@$image,$label);
 }
 $image->Set(delay=>20);
