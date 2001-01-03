20c50b4297db2b5729d9b3334ef49212efdae12b
John Cristy <duc.sequere.aut.de.via.decede@gmail.com>
2001-01-03 16:07:01 -0600

    *** empty log message ***

--- magick/cache.c
+++ magick/cache.c
@@ -893,7 +893,7 @@ MagickExport unsigned int OpenCache(Cache cache,const ClassType storage_class,
       if (getenv("MAGICK_CACHE_THRESHOLD") != (char *) NULL)
         threshold=atoi(getenv("MAGICK_CACHE_THRESHOLD"));
 #if defined(_SC_PAGESIZE) && defined(_SC_PHYS_PAGES)
-      if (threshold > (sysconf(_SC_PAGESIZE)*sysconf(_SC_PHYS_PAGES/1048576)))
+      if (threshold > (sysconf(_SC_PAGESIZE)*sysconf(_SC_PHYS_PAGES)/1048576))
         threshold=sysconf(_SC_PAGESIZE)*sysconf(_SC_PHYS_PAGES)/1048576;
 #endif
       SetCacheThreshold(threshold);
