0f5ab339b3657ab80f41f75c913f0d55bd2df597
John Cristy <duc.sequere.aut.de.via.decede@gmail.com>
2001-02-07 22:00:52 -0600

    *** empty log message ***

--- utilities/mogrify.c
+++ utilities/mogrify.c
@@ -1149,12 +1149,9 @@ int main(int argc,char **argv)
             }
           if (LocaleNCompare("profile",option+1,4) == 0)
             {
-              if (*option == '-')
-                {
-                  i++;
-                  if (i == argc)
-                    MagickError(OptionError,"Missing profile",option);
-                }
+              i++;
+              if (i == argc)
+                MagickError(OptionError,"Missing profile",option);
               break;
             }
           MagickError(OptionError,"Unrecognized option",option);
