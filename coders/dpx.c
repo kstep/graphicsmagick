cdb1984283aacd95ee1c68aaea13dfc129e87b1f
John Cristy <duc.sequere.aut.de.via.decede@gmail.com>
2001-03-07 13:48:18 -0600

    *** empty log message ***

---
+++ coders/dpx.c
@@ -0,0 +1,234 @@
+/*
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                             %
+%                                                                             %
+%                                                                             %
+%                            DDDD   PPPP   SSSSS                              %
+%                            D   D  P   P  SS                                 %
+%                            D   D  PPPP    SSS                               %
+%                            D   D  P         SS                              %
+%                            DDDD   P      SSSSS                              %
+%                                                                             %
+%                                                                             %
+%                    Read/Write ImageMagick Image Format.                     %
+%                                                                             %
+%                                                                             %
+%                              Software Design                                %
+%                                John Cristy                                  %
+%                                March 2001                                   %
+%                                                                             %
+%                                                                             %
+%  Copyright (C) 2001 ImageMagick Studio, a non-profit organization dedicated %
+%  to making software imaging solutions freely available.                     %
+%                                                                             %
+%  Permission is hereby granted, free of charge, to any person obtaining a    %
+%  copy of this software and associated documentation files ("ImageMagick"),  %
+%  to deal in ImageMagick without restriction, including without limitation   %
+%  the rights to use, copy, modify, merge, publish, distribute, sublicense,   %
+%  and/or sell copies of ImageMagick, and to permit persons to whom the       %
+%  ImageMagick is furnished to do so, subject to the following conditions:    %
+%                                                                             %
+%  The above copyright notice and this permission notice shall be included in %
+%  all copies or substantial portions of ImageMagick.                         %
+%                                                                             %
+%  The software is provided "as is", without warranty of any kind, express or %
+%  implied, including but not limited to the warranties of merchantability,   %
+%  fitness for a particular purpose and noninfringement.  In no event shall   %
+%  ImageMagick Studio be liable for any claim, damages or other liability,    %
+%  whether in an action of contract, tort or otherwise, arising from, out of  %
+%  or in connection with ImageMagick or the use or other dealings in          %
+%  ImageMagick.                                                               %
+%                                                                             %
+%  Except as contained in this notice, the name of the ImageMagick Studio     %
+%  shall not be used in advertising or otherwise to promote the sale, use or  %
+%  other dealings in ImageMagick without prior written authorization from the %
+%  ImageMagick Studio.                                                        %
+%                                                                             %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%
+*/
+
+/*
+  Include declarations.
+*/
+#include "magick.h"
+#include "defines.h"
+
+/*
+  Forward declarations.
+*/
+static unsigned int
+  WriteDPXImage(const ImageInfo *,Image *);
+
+/*
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                             %
+%                                                                             %
+%                                                                             %
+%   R e a d D P X I m a g e                                                   %
+%                                                                             %
+%                                                                             %
+%                                                                             %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%  Method ReadDPXImage reads an DPX X image file and returns it.  It
+%  allocates the memory necessary for the new Image structure and returns a
+%  pointer to the new image.
+%
+%  The format of the ReadDPXImage method is:
+%
+%      Image *ReadDPXImage(const ImageInfo *image_info,ExceptionInfo *exception)
+%
+%  A description of each parameter follows:
+%
+%    o image:  Method ReadDPXImage returns a pointer to the image after
+%      reading. A null image is returned if there is a memory shortage or if
+%      the image cannot be read.
+%
+%    o image_info: Specifies a pointer to an ImageInfo structure.
+%
+%    o exception: return any errors or warnings in this structure.
+%
+%
+*/
+static Image *ReadDPXImage(const ImageInfo *image_info,ExceptionInfo *exception)
+{
+  char
+    magick[4];
+
+  Image
+    *image;
+
+  int
+    y;
+
+  register int
+    i,
+    x;
+
+  register PixelPacket
+    *q;
+
+  unsigned int
+    status;
+
+  unsigned long
+    height,
+    pixel,
+    width;
+
+  /*
+    Open image file.
+  */
+  image=AllocateImage(image_info);
+  status=OpenBlob(image_info,image,ReadBinaryType);
+  if (status == False)
+    ThrowReaderException(FileOpenWarning,"Unable to open file",image);
+  /*
+    Read DPX image.
+  */
+  status=ReadBlob(image,4,(char *) magick);
+  if ((status == False) || (LocaleNCompare((char *) magick,"SDPX",4) != 0))
+    ThrowReaderException(CorruptImageWarning,"Not a DPX image file",image);
+  for (i=0; i < 768; i++)
+    (void) ReadBlobByte(image);
+  width=ReadBlobMSBLong(image);
+  height=ReadBlobMSBLong(image);
+  for (i=0; i < 7412; i++)
+    (void) ReadBlobByte(image);
+  /*
+    Initialize image structure.
+  */
+  image->columns=width;
+  image->rows=height;
+  image->depth=8;
+  if (image_info->ping)
+    {
+      CloseBlob(image);
+      return(image);
+    }
+  /*
+    Convert DPX raster image to pixel packets.
+  */
+  for (y=0; y < (int) image->rows; y++)
+  {
+    q=SetImagePixels(image,0,y,image->columns,1);
+    if (q == (PixelPacket *) NULL)
+      break;
+    for (x=0; x < (int) image->columns; x++)
+    {
+      pixel=ReadBlobMSBLong(image);
+      q->red=MaxRGB*((pixel >> 22) & 0x3ff)/1023;
+      q->green=MaxRGB*((pixel >> 12) & 0x3ff)/1023;
+      q->blue=MaxRGB*((pixel >> 2) & 0x3ff)/1023;
+      q++;
+    }
+    if (!SyncImagePixels(image))
+      break;
+    if (image->previous == (Image *) NULL)
+      if (QuantumTick(y,image->rows))
+        MagickMonitor(LoadImageText,y,image->rows);
+  }
+  CloseBlob(image);
+  return(image);
+}
+
+/*
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                             %
+%                                                                             %
+%                                                                             %
+%   R e g i s t e r D P X I m a g e                                           %
+%                                                                             %
+%                                                                             %
+%                                                                             %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%  Method RegisterDPXImage adds attributes for the DPX image format to
+%  the list of supported formats.  The attributes include the image format
+%  tag, a method to read and/or write the format, whether the format
+%  supports the saving of more than one frame to the same file or blob,
+%  whether the format supports native in-memory I/O, and a brief
+%  description of the format.
+%
+%  The format of the RegisterDPXImage method is:
+%
+%      RegisterDPXImage(void)
+%
+*/
+ModuleExport void RegisterDPXImage(void)
+{
+  MagickInfo
+    *entry;
+
+  entry=SetMagickInfo("DPX");
+  entry->decoder=ReadDPXImage;
+  entry->description=AllocateString("DPX image");
+  entry->module=AllocateString("DPX");
+  RegisterMagickInfo(entry);
+}
+
+/*
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                             %
+%                                                                             %
+%                                                                             %
+%   U n r e g i s t e r D P X I m a g e                                       %
+%                                                                             %
+%                                                                             %
+%                                                                             %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%  Method UnregisterDPXImage removes format registrations made by the
+%  DPX module from the list of supported formats.
+%
+%  The format of the UnregisterDPXImage method is:
+%
+%      UnregisterDPXImage(void)
+%
+*/
+ModuleExport void UnregisterDPXImage(void)
+{
+  UnregisterMagickInfo("DPX");
+}
