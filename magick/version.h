d410b258fe7d85f8b18292579ee689f5041b41f4
John Cristy <duc.sequere.aut.de.via.decede@gmail.com>
2001-06-26 09:51:10 -0500

    *** empty log message ***

--- magick/version.h
+++ magick/version.h
@@ -9,13 +9,13 @@ extern "C" {
 #endif
 
 #define MagickCopyright  "Copyright (C) 2001 ImageMagick Studio"
-#define MagickLibVersion  0x0535
+#define MagickLibVersion  0x0536
 #if defined(QuantumLeap)
 #define MagickVersion \
-  "@(#)ImageMagick 5.3.5 07/01/01 Q:16 http://www.imagemagick.org"
+  "@(#)ImageMagick 5.3.6 07/01/01 Q:16 http://www.imagemagick.org"
 #else
 #define MagickVersion \
-  "@(#)ImageMagick 5.3.5 07/01/01 Q:8 http://www.imagemagick.org"
+  "@(#)ImageMagick 5.3.6 07/01/01 Q:8 http://www.imagemagick.org"
 #endif
 
 #if defined(__cplusplus) || defined(c_plusplus)
