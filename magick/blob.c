816859c398764f0f65fc1ceb8745c2f6f6024338
John Cristy <duc.sequere.aut.de.via.decede@gmail.com>
2001-07-30 15:29:01 -0500

    *** empty log message ***

--- magick/blob.c
+++ magick/blob.c
@@ -216,7 +216,9 @@ MagickExport void CloseBlob(Image *image)
     }
   if (image->fifo != (int (*)(const Image *,const void *,const size_t)) NULL)
     {
-      /* signal that all data has been written to the stream */
+      /*
+        Signal that all data has been written to the stream.
+      */
       (void) image->fifo(image,(const void *) NULL,0);
       image->fifo=(int (*)(const Image *,const void *,const size_t)) NULL;
       return;
