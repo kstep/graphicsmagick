4bc31ae8afa24b3a1c7cfce2f02123effae8a067
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2001-09-23 18:58:28 -0500

    Allow copy of CoderInfo so it can be put in list.

--- Magick++/lib/Magick++/CoderInfo.h
+++ Magick++/lib/Magick++/CoderInfo.h
@@ -54,7 +54,7 @@ namespace Magick
     CoderInfo::CoderInfo ( void );
 
     // Copy constructor (not supported)
-    CoderInfo::CoderInfo ( const CoderInfo &coder_ );
+//     CoderInfo::CoderInfo ( const CoderInfo &coder_ );
 
     // Assignment operator (not supported)
     CoderInfo& operator= (const CoderInfo &coder_ );
