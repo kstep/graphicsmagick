0077f763079d17eafe9decfe07a0d401e82253b4
John Cristy <duc.sequere.aut.de.via.decede@gmail.com>
2001-09-29 19:08:29 -0500

    *** empty log message ***

--- magick/cache.c
+++ magick/cache.c
@@ -62,7 +62,7 @@
 /*
   Define declarations.
 */
-#define DefaultCacheMemory  (((off_t) 1L << (8*sizeof(off_t)))-1)
+#define DefaultCacheMemory  ((off_t) 1L << (8*sizeof(off_t)-1)-1)
 
 /*
   Global declarations.
