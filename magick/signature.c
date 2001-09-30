a27e7a0b6758fc5aed8a67fa0175a664e62fd5e2
John Cristy <duc.sequere.aut.de.via.decede@gmail.com>
2001-09-30 15:21:10 -0500

    *** empty log message ***

--- magick/signature.c
+++ magick/signature.c
@@ -134,11 +134,11 @@ static void FinalizeSignature(SignatureInfo *signature_info)
   signature_info->message[56]=(high_order >> 24) & 0xff;
   signature_info->message[57]=(high_order >> 16) & 0xff;
   signature_info->message[58]=(high_order >> 8) & 0xff;
-  signature_info->message[59]=(high_order >> 0) & 0xff;
+  signature_info->message[59]=high_order & 0xff;
   signature_info->message[60]=(low_order >> 24) & 0xff;
   signature_info->message[61]=(low_order >> 16) & 0xff;
   signature_info->message[62]=(low_order >> 8) & 0xff;
-  signature_info->message[63]=(low_order >> 0) & 0xff;
+  signature_info->message[63]=low_order & 0xff;
   TransformSignature(signature_info);
 }
 
@@ -167,6 +167,7 @@ static void FinalizeSignature(SignatureInfo *signature_info)
 */
 static void GetSignatureInfo(SignatureInfo *signature_info)
 {
+  (void) memset(signature_info,0,sizeof(SignatureInfo));
   signature_info->digest[0]=0x6a09e667UL;
   signature_info->digest[1]=0xbb67ae85UL;
   signature_info->digest[2]=0x3c6ef372UL;
@@ -175,10 +176,6 @@ static void GetSignatureInfo(SignatureInfo *signature_info)
   signature_info->digest[5]=0x9b05688cUL;
   signature_info->digest[6]=0x1f83d9abUL;
   signature_info->digest[7]=0x5be0cd19UL;
-  signature_info->low_order=0L;
-  signature_info->high_order=0L;
-  signature_info->offset=0;
-  (void) memset(signature_info->message,0,SignatureSize);
 }
 
 /*
@@ -217,21 +214,18 @@ static void TransformSignature(SignatureInfo *signature_info)
   static unsigned long
     K[64] =
     {
-      0x428a2f98UL, 0x71374491UL, 0xb5c0fbcfUL, 0xe9b5dba5UL,
-      0x3956c25bUL, 0x59f111f1UL, 0x923f82a4UL, 0xab1c5ed5UL,
-      0xd807aa98UL, 0x12835b01UL, 0x243185beUL, 0x550c7dc3UL,
-      0x72be5d74UL, 0x80deb1feUL, 0x9bdc06a7UL, 0xc19bf174UL,
-      0xe49b69c1UL, 0xefbe4786UL, 0x0fc19dc6UL, 0x240ca1ccUL,
-      0x2de92c6fUL, 0x4a7484aaUL, 0x5cb0a9dcUL, 0x76f988daUL,
-      0x983e5152UL, 0xa831c66dUL, 0xb00327c8UL, 0xbf597fc7UL,
-      0xc6e00bf3UL, 0xd5a79147UL, 0x06ca6351UL, 0x14292967UL,
-      0x27b70a85UL, 0x2e1b2138UL, 0x4d2c6dfcUL, 0x53380d13UL,
-      0x650a7354UL, 0x766a0abbUL, 0x81c2c92eUL, 0x92722c85UL,
-      0xa2bfe8a1UL, 0xa81a664bUL, 0xc24b8b70UL, 0xc76c51a3UL,
-      0xd192e819UL, 0xd6990624UL, 0xf40e3585UL, 0x106aa070UL,
-      0x19a4c116UL, 0x1e376c08UL, 0x2748774cUL, 0x34b0bcb5UL,
-      0x391c0cb3UL, 0x4ed8aa4aUL, 0x5b9cca4fUL, 0x682e6ff3UL,
-      0x748f82eeUL, 0x78a5636fUL, 0x84c87814UL, 0x8cc70208UL,
+      0x428a2f98UL, 0x71374491UL, 0xb5c0fbcfUL, 0xe9b5dba5UL, 0x3956c25bUL,
+      0x59f111f1UL, 0x923f82a4UL, 0xab1c5ed5UL, 0xd807aa98UL, 0x12835b01UL,
+      0x243185beUL, 0x550c7dc3UL, 0x72be5d74UL, 0x80deb1feUL, 0x9bdc06a7UL,
+      0xc19bf174UL, 0xe49b69c1UL, 0xefbe4786UL, 0x0fc19dc6UL, 0x240ca1ccUL,
+      0x2de92c6fUL, 0x4a7484aaUL, 0x5cb0a9dcUL, 0x76f988daUL, 0x983e5152UL,
+      0xa831c66dUL, 0xb00327c8UL, 0xbf597fc7UL, 0xc6e00bf3UL, 0xd5a79147UL,
+      0x06ca6351UL, 0x14292967UL, 0x27b70a85UL, 0x2e1b2138UL, 0x4d2c6dfcUL,
+      0x53380d13UL, 0x650a7354UL, 0x766a0abbUL, 0x81c2c92eUL, 0x92722c85UL,
+      0xa2bfe8a1UL, 0xa81a664bUL, 0xc24b8b70UL, 0xc76c51a3UL, 0xd192e819UL,
+      0xd6990624UL, 0xf40e3585UL, 0x106aa070UL, 0x19a4c116UL, 0x1e376c08UL,
+      0x2748774cUL, 0x34b0bcb5UL, 0x391c0cb3UL, 0x4ed8aa4aUL, 0x5b9cca4fUL,
+      0x682e6ff3UL, 0x748f82eeUL, 0x78a5636fUL, 0x84c87814UL, 0x8cc70208UL,
       0x90befffaUL, 0xa4506cebUL, 0xbef9a3f7UL, 0xc67178f2UL
     };  /* 32-bit fractional part of the cube root of the first 64 primes */
 
