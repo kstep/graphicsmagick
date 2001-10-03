afbc9ae8c9429037207c24877fde60e332de0e24
John Cristy <duc.sequere.aut.de.via.decede@gmail.com>
2001-10-02 21:10:30 -0500

    *** empty log message ***

--- PerlMagick/demo/shadow_text.pl
+++ PerlMagick/demo/shadow_text.pl
@@ -9,7 +9,7 @@ $image->Read('xc:white');
 $image->Annotate(font=>'@Generic.ttf',fill=>'rgb(100,100,100)',pointsize=>60,
   text=>'Works like magick!',geometry=>'+30+90');
 $image->Blur('0x1');
-$image->Annotate(font=>'@Generic.ttf',fill=>'red',pointsize=>60,
+$image->Annotate(font=>'@Generic.ttf',fill=>'red',stroke=>'blue',pointsize=>60,
   text=>'Works like magick!',geometry=>'+26+86');
 $mask=$image->Clone();
 $image->Write('shadow.gif');
