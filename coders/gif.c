d02275ba1fafa604a5aaf320d16d1e0ac861f849
John Cristy <duc.sequere.aut.de.via.decede@gmail.com>
2001-10-18 12:24:45 -0500

    *** empty log message ***

--- coders/gif.c
+++ coders/gif.c
@@ -1177,7 +1177,7 @@ static unsigned int WriteGIFImage(const ImageInfo *image_info,Image *image)
   do
   {
     (void) TransformRGBImage(image,RGBColorspace);
-    if (!IsPaletteImage(image,&image->exception))
+    if ((image->storage_class == DirectClass) || (image->colors > 256))
       {
         /*
           GIF requires that the image is colormapped.
