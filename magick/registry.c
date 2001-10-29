481e2cf2d2332d18d4063cbb4b6c449726413474
John Cristy <duc.sequere.aut.de.via.decede@gmail.com>
2001-10-29 09:36:10 -0600

    *** empty log message ***

--- magick/registry.c
+++ magick/registry.c
@@ -1,50 +1,50 @@
 /*
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%                                                                           %
-%                                                                           %
-%                                                                           %
-%           RRRR    EEEEE    GGG   IIIII  SSSSS  TTTTT  RRRR   Y   Y        %
-%           R   R   E       G        I    SS       T    R   R   Y Y         %
-%           RRRR    EEE     G GGG    I     SSS     T    RRRR     Y          %
-%           R R     E       G   G    I       SS    T    R R      Y          %
-%           R  R    EEEEE    GGG   IIIII  SSSSS    T    R  R     Y          %
-%                                                                           %
-%                                                                           %
-%                            ImageMagick Registry.                          %
-%                                                                           %
-%                                                                           %
-%                              Software Design                              %
-%                                John Cristy                                %
-%                                 July 1992                                 %
-%                                                                           %
-%                                                                           %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                             %
+%                                                                             %
+%                                                                             %
+%           RRRR    EEEEE    GGG   IIIII  SSSSS  TTTTT  RRRR   Y   Y          %
+%           R   R   E       G        I    SS       T    R   R   Y Y           %
+%           RRRR    EEE     G GGG    I     SSS     T    RRRR     Y            %
+%           R R     E       G   G    I       SS    T    R R      Y            %
+%           R  R    EEEEE    GGG   IIIII  SSSSS    T    R  R     Y            %
+%                                                                             %
+%                                                                             %
+%                            ImageMagick Registry.                            %
+%                                                                             %
+%                                                                             %
+%                              Software Design                                %
+%                                John Cristy                                  %
+%                                October 2001                                 %
+%                                                                             %
+%                                                                             %
 %  Copyright (C) 2001 ImageMagick Studio, a non-profit organization dedicated %
-%  to making software imaging solutions freely available.                   %
-%                                                                           %
-%  Permission is hereby granted, free of charge, to any person obtaining a  %
-%  copy of this software and associated documentation files ("ImageMagick"),%
-%  to deal in ImageMagick without restriction, including without limitation %
-%  the rights to use, copy, modify, merge, publish, distribute, sublicense, %
-%  and/or sell copies of ImageMagick, and to permit persons to whom the     %
-%  ImageMagick is furnished to do so, subject to the following conditions:  %
-%                                                                           %
+%  to making software imaging solutions freely available.                     %
+%                                                                             %
+%  Permission is hereby granted, free of charge, to any person obtaining a    %
+%  copy of this software and associated documentation files ("ImageMagick"),  %
+%  to deal in ImageMagick without restriction, including without limitation   %
+%  the rights to use, copy, modify, merge, publish, distribute, sublicense,   %
+%  and/or sell copies of ImageMagick, and to permit persons to whom the       %
+%  ImageMagick is furnished to do so, subject to the following conditions:    %
+%                                                                             %
 %  The above copyright notice and this permission notice shall be included in %
-%  all copies or substantial portions of ImageMagick.                       %
-%                                                                           %
+%  all copies or substantial portions of ImageMagick.                         %
+%                                                                             %
 %  The software is provided "as is", without warranty of any kind, express or %
-%  implied, including but not limited to the warranties of merchantability, %
-%  fitness for a particular purpose and noninfringement.  In no event shall %
-%  ImageMagick Studio be liable for any claim, damages or other liability,  %
-%  whether in an action of contract, tort or otherwise, arising from, out of%
-%  or in connection with ImageMagick or the use or other dealings in        %
-%  ImageMagick.                                                             %
-%                                                                           %
-%  Except as contained in this notice, the name of the ImageMagick Studio   %
-%  shall not be used in advertising or otherwise to promote the sale, use or%
+%  implied, including but not limited to the warranties of merchantability,   %
+%  fitness for a particular purpose and noninfringement.  In no event shall   %
+%  ImageMagick Studio be liable for any claim, damages or other liability,    %
+%  whether in an action of contract, tort or otherwise, arising from, out of  %
+%  or in connection with ImageMagick or the use or other dealings in          %
+%  ImageMagick.                                                               %
+%                                                                             %
+%  Except as contained in this notice, the name of the ImageMagick Studio     %
+%  shall not be used in advertising or otherwise to promote the sale, use or  %
 %  other dealings in ImageMagick without prior written authorization from the %
-%  ImageMagick Studio.                                                      %
-%                                                                           %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  ImageMagick Studio.                                                        %
+%                                                                             %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 %
 */
