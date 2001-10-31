6d952b036c0484f89e5466a806cc2b8c44827510
John Cristy <duc.sequere.aut.de.via.decede@gmail.com>
2001-10-31 09:31:12 -0600

    *** empty log message ***

--- coders/bmp.c
+++ coders/bmp.c
@@ -1088,10 +1088,10 @@ static unsigned int WriteBMPImage(const ImageInfo *image_info,Image *image)
         /*
           Convert PseudoClass image to a BMP monochrome image.
         */
-        polarity=Intensity(image->colormap[0]) > (MaxRGB >> 1);
+        polarity=Intensity(image->colormap[0]) < (MaxRGB >> 1);
         if (image->colors == 2)
           polarity=
-            Intensity(image->colormap[1]) > Intensity(image->colormap[0]);
+            Intensity(image->colormap[1]) < Intensity(image->colormap[0]);
         for (y=0; y < (long) image->rows; y++)
         {
           p=AcquireImagePixels(image,0,y,image->columns,1,&image->exception);
