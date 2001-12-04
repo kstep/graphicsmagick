c3876db1c0e59462b5c5e79f2ca606a350dcd0d2
John Cristy <duc.sequere.aut.de.via.decede@gmail.com>
2001-12-04 10:07:37 -0600

    *** empty log message ***

--- magick/version.h
+++ magick/version.h
@@ -8,18 +8,18 @@
 extern "C" {
 #endif
 
-#define MagickCopyright  "Copyright (C) 2001 ImageMagick Studio"
-#define MagickLibVersion  0x0541
-#define MagickLibVersionText  "5.4.1"
-#define MagickLibVersionNumber  5.4.1,0
+#define MagickCopyright  "Copyright (C) 2002 ImageMagick Studio"
+#define MagickLibVersion  0x0542
+#define MagickLibVersionText  "5.4.2"
+#define MagickLibVersionNumber  5.4.2,0
 #if QuantumDepth == 8
 #define MagickVersion \
-  "@(#)ImageMagick 5.4.1 12/01/01 Q:8 http://www.imagemagick.org"
+  "@(#)ImageMagick 5.4.2 01/01/02 Q:8 http://www.imagemagick.org"
 #elif QuantumDepth == 16
 #define MagickVersion \
-  "@(#)ImageMagick 5.4.1 12/01/01 Q:16 http://www.imagemagick.org"
+  "@(#)ImageMagick 5.4.2 01/01/02 Q:16 http://www.imagemagick.org"
 #else
-#define MagickVersion "@(#)ImageMagick 5.4.1 12/01/01 http://www.imagemagick.org"
+#define MagickVersion "@(#)ImageMagick 5.4.2 12/01/01 http://www.imagemagick.org"
 #endif
 
 #if defined(__cplusplus) || defined(c_plusplus)
