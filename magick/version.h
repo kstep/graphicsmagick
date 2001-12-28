74aaa200e7a730778bbce8e50e7fd2c0c3975714
John Cristy <duc.sequere.aut.de.via.decede@gmail.com>
2001-12-28 10:09:03 -0600

    *** empty log message ***

--- magick/version.h
+++ magick/version.h
@@ -8,7 +8,7 @@
 extern "C" {
 #endif
 
-#define MagickCopyright  "Copyright (C) 2002 ImageMagick Studio"
+#define MagickCopyright  "Copyright (C) 2002 ImageMagick Studio LLC"
 #define MagickLibVersion  0x0542
 #define MagickLibVersionText  "5.4.2"
 #define MagickLibVersionNumber  5.4.2,0
