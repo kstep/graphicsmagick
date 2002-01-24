d3beb5fe7bbb0593ff0acbdc1555843b1b3124a5
John Cristy <duc.sequere.aut.de.via.decede@gmail.com>
2002-01-23 22:26:36 -0600

    *** empty log message ***

--- magick/annotate.c
+++ magick/annotate.c
@@ -1309,7 +1309,6 @@ static unsigned int RenderTruetype(Image *image,const DrawInfo *draw_info,
   metrics->bounds.y1/=64.0;
   metrics->bounds.x2/=64.0;
   metrics->bounds.y2/=64.0;
-  metrics->bounds.y1,metrics->bounds.x2,metrics->bounds.y2);
   if (render)
     if ((draw_info->stroke.opacity != TransparentOpacity) ||
         (draw_info->stroke_pattern != (Image *) NULL))
