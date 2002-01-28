35427a800d86a95e6bb833258a8a3d76b6e1235e
Glenn Randers-Pehrson <glennrp@comcast.net>
2002-01-27 18:23:46 -0600

    Backing out previous update.

--- magick/image.c
+++ magick/image.c
@@ -5223,8 +5223,6 @@ MagickExport int ParseImageGeometry(const char *geometry,long *x,long *y,
     *x+=former_width-(*width);
   if ((flags & YValue) == 0)
     *height-=(*y) << 1;
-  if ((flags & XNegative) != 0)
-    *x+=former_width-(*width);
   if ((flags & YNegative) != 0)
     *y+=former_height-(*height);
   if (flags & GreaterValue)
