6fc16feffbe790f11d3c3cc6030772fadf157fbe
John Cristy <duc.sequere.aut.de.via.decede@gmail.com>
2002-03-05 20:46:38 -0600

    *** empty log message ***

--- magick/annotate.c
+++ magick/annotate.c
@@ -1368,8 +1368,8 @@ static unsigned int RenderTruetype(Image *image,const DrawInfo *draw_info,
           */
           clone_info->affine.tx=glyph.origin.x/64.0;
           clone_info->affine.ty=glyph.origin.y/64.0;
-          (void) FT_Outline_Decompose(&((FT_OutlineGlyph)
-            glyph.image)->outline,&OutlineMethods,clone_info);
+          (void) FT_Outline_Decompose(&((FT_OutlineGlyph) glyph.image)->outline,
+            &OutlineMethods,clone_info);
         }
     FT_Vector_Transform(&glyph.origin,&affine);
     (void) FT_Glyph_Transform(glyph.image,&affine,&glyph.origin);
