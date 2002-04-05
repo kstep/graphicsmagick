6d1d732301fea71e1b409d2c693dc0c23ea352ac
Leonard Rosenthol <leonardr@lazerware.com>
2002-04-05 10:09:17 -0600

    Added fix for color correction to CMYK JPEG's

--- coders/pdf.c
+++ coders/pdf.c
@@ -1103,7 +1103,17 @@ static unsigned int WritePDFImage(const ImageInfo *image_info,Image *image)
     switch (compression)
     {
       case NoCompression: FormatString(buffer,CFormat,"ASCII85Decode"); break;
-      case JPEGCompression: FormatString(buffer,CFormat,"DCTDecode"); break;
+      case JPEGCompression: 
+		  {
+			  FormatString(buffer,CFormat,"DCTDecode"); 
+              (void) WriteBlobString(image,buffer);
+			  if ( image->colorspace == CMYKColorspace )
+			  {
+				  strcpy( buffer, "/Decode[1 0 1 0 1 0 1 0]\n" );
+                  (void) WriteBlobString(image,buffer);
+			  }
+			  break;
+		  }
       case LZWCompression: FormatString(buffer,CFormat,"LZWDecode"); break;
       case ZipCompression: FormatString(buffer,CFormat,"FlateDecode"); break;
       case FaxCompression:
