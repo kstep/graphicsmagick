5721151232d346c7b22c75ff498e1ea2488a5004
John Cristy <duc.sequere.aut.de.via.decede@gmail.com>
2002-04-12 08:27:00 -0500

    *** empty log message ***

--- magick/error.c
+++ magick/error.c
@@ -447,8 +447,8 @@ MagickExport void MagickError(const ExceptionType error,const char *reason,
 MagickExport void MagickFatalError(const ExceptionType error,const char *reason,
   const char *description)
 {
-  if (error_handler != (ErrorHandler) NULL)
-    (*error_handler)(error,reason,description);
+  if (fatal_error_handler != (ErrorHandler) NULL)
+    (*fatal_error_handler)(error,reason,description);
   errno=0;
 }
 
