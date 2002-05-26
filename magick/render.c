15ba56451104c849ef73ee6fe4e55c6ec2110c38
John Cristy <duc.sequere.aut.de.via.decede@gmail.com>
2002-05-25 21:26:32 -0500

    *** empty log message ***

--- magick/render.c
+++ magick/render.c
@@ -1731,7 +1731,7 @@ static inline unsigned int IsPoint(const char *point)
 
 MagickExport unsigned int DrawImage(Image *image,const DrawInfo *draw_info)
 {
-#define DrawImageText  "  Drawing image...  "
+#define RenderImageText  "  Render image...  "
 
   AffineMatrix
     affine,
@@ -3202,7 +3202,7 @@ MagickExport unsigned int DrawImage(Image *image,const DrawInfo *draw_info)
     (void) DrawPrimitive(image,graphic_context[n],primitive_info);
     if (primitive_info->text != (char *) NULL)
       LiberateMemory((void **) &primitive_info->text);
-    MagickMonitor(DrawImageText,(double) (q-primitive),primitive_extent);
+    MagickMonitor(RenderImageText,(double) (q-primitive),primitive_extent);
   }
   if (graphic_context[n]->debug)
     (void) fprintf(stdout,"end draw-image (%.2fu)\n",GetUserTime(&timer));
