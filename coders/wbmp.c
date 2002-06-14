8bf834e8b9443d76de1633bd883fecc14ca66762
John Cristy <duc.sequere.aut.de.via.decede@gmail.com>
2002-06-14 13:26:52 -0500

    *** empty log message ***

--- coders/wbmp.c
+++ coders/wbmp.c
@@ -376,8 +376,8 @@ static unsigned int WriteWBMPImage(const ImageInfo *image_info,Image *image)
   /*
     Convert image to a bi-level image.
   */
-  if (!IsMonochromeImage(image,&image->exception) || image->storage_class
-      != PseudoClass)
+  if ((image->storage_class == DirectClass) ||
+    	!IsMonochromeImage(image,&image->exception))
     {
       QuantizeInfo
         quantize_info;
