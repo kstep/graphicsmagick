d04beff0b89fbf6abd05785dc3c6884e176dac87
John Cristy <duc.sequere.aut.de.via.decede@gmail.com>
2002-06-20 11:47:31 -0500

    *** empty log message ***

--- magick/studio.h
+++ magick/studio.h
@@ -11,8 +11,13 @@ extern "C" {
 /*
   System include declarations.
 */
-#define _XOPEN_SOURCE  500
-#define _POSIX_C_SOURCE  199506L
+#define _GNU_SOURCE  1
+#define _ISOC99_SOURCE  1
+#define _POSIX_SOURCE  1
+#define _POSIX_C_SOURCE  199506
+#define _XOPEN_SOURCE  600 
+#define _XOPEN_SOURCE_EXTENDED  1
+#define _LARGEFILE64_SOURCE  1
 
 #if !defined(_MAGICK_CONFIG_H)
 # define _MAGICK_CONFIG_H
