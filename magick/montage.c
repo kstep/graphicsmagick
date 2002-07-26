44131aa4733ac24fad6ded3dacd6209135bb3c18
John Cristy <duc.sequere.aut.de.via.decede@gmail.com>
2002-07-26 09:20:59 -0500

    *** empty log message ***

--- magick/montage.c
+++ magick/montage.c
@@ -530,7 +530,7 @@ MagickExport Image *MontageImages(const Image *images,
           max_height=0;
         }
     }
-    bounds.height-=(concatenate ? 0 : 4);
+    bounds.height-=(concatenate ? 0 : 2);
     /*
       Initialize montage image.
     */
