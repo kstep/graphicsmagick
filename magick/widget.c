3181c769da0f77efdbb8d3303c9b6d31922f4427
John Cristy <duc.sequere.aut.de.via.decede@gmail.com>
2002-08-11 17:33:21 -0500

    *** empty log message ***

--- magick/widget.c
+++ magick/widget.c
@@ -1475,7 +1475,7 @@ static void XSetMatteColor(Display *display,const XWindowInfo *window_info,
 static void XSetTextColor(Display *display,const XWindowInfo *window_info,
   const unsigned int raised)
 {
-  int
+  unsigned long
     foreground,
     matte;
 
@@ -1492,9 +1492,9 @@ static void XSetTextColor(Display *display,const XWindowInfo *window_info,
           XWhitePixel(display,window_info->screen));
       return;
     }
-  foreground=(int) ScaleShortToQuantum(
+  foreground=ScaleShortToQuantum(
     PixelIntensity(&window_info->pixel_info->foreground_color));
-  matte=(int) ScaleShortToQuantum(
+  matte=ScaleShortToQuantum(
     PixelIntensity(&window_info->pixel_info->matte_color));
   if (AbsoluteValue(foreground-matte) > (MaxRGB >> 3))
     (void) XSetForeground(display,window_info->widget_context,
