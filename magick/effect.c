000b2104c724060cf67ff4467a2ae6b6c5a4393e
John Cristy <duc.sequere.aut.de.via.decede@gmail.com>
2002-08-15 15:24:13 -0500

    *** empty log message ***

--- magick/effect.c
+++ magick/effect.c
@@ -2986,8 +2986,8 @@ MagickExport Image *SpreadImage(const Image *image,const unsigned int radius,
     {
       do
       {
-        x_distance=(long) (((2.0*radius+1.0)*rand())/RAND_MAX)-quantum;
-        y_distance=(long) (((2.0*radius+1.0)*rand())/RAND_MAX)-quantum;
+        x_distance=(((2*(long) radius+1)*rand())/RAND_MAX)-quantum;
+        y_distance=(((2*(long) radius+1)*rand())/RAND_MAX)-quantum;
       } while (((x+x_distance) < 0) || ((y+y_distance) < 0) ||
                ((x+x_distance) >= (long) image->columns) ||
                ((y+y_distance) >= (long) image->rows));
