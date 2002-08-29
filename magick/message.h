8dee914b6f3ec163de559d9778d0dfb0e41c0bd6
John Cristy <duc.sequere.aut.de.via.decede@gmail.com>
2002-08-29 11:30:59 -0500

    *** empty log message ***

---
+++ magick/message.h
@@ -0,0 +1,16 @@
+/*
+  Image Message Methods.
+*/
+#ifndef _MAGICK_MESSAGE_H
+#define _MAGICK_MESSAGE_H
+
+#if defined(__cplusplus) || defined(c_plusplus)
+extern "C" {
+#endif
+
+
+#if defined(__cplusplus) || defined(c_plusplus)
+}
+#endif
+
+#endif
