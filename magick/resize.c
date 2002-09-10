9025b1efd99ed6d16a254dd74c6146d189424cf9
John Cristy <duc.sequere.aut.de.via.decede@gmail.com>
2002-09-09 20:14:31 -0500

    *** empty log message ***

--- magick/resize.c
+++ magick/resize.c
@@ -153,8 +153,8 @@ static double J1(const double x)
   q=Qone[8];
   for (i=7; i >= 0; i--)
   {
-    p=p*x*x+Pone[i];
-    q=q*x*x+Qone[i];
+    p*=x*x+Pone[i];
+    q*=x*x+Qone[i];
   }
   return(p/q);
 }
