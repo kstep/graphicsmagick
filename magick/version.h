b3a027e7b150329a90d6007f183adbfb4a0b3a80
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2002-09-09 23:15:53 -0500

    Windows RC files and possibly other places need access to text strings.

--- magick/version.h
+++ magick/version.h
@@ -9,6 +9,27 @@ extern "C" {
 #endif
 
 /*
+  Define declarations.
+*/
+#define MagickCopyright  "Copyright (C) 2002 ImageMagick Studio LLC"
+#define MagickLibVersion  0x0551
+#define MagickLibVersionText  "5.5.1"
+#define MagickLibVersionNumber  5,5,1,0
+#if (QuantumDepth == 8)
+#define MagickQuantumDepth  "Q8"
+#elif (QuantumDepth == 16)
+#define MagickQuantumDepth  "Q16"
+#elif (QuantumDepth == 32)
+#define MagickQuantumDepth  "Q32"
+#else
+#define MagickQuantumDepth  "Q?"
+#endif
+#define MagickReleaseDate  "10/01/02"
+#define MagickVersion "ImageMagick " MagickLibVersionText " " \
+  MagickReleaseDate " " MagickQuantumDepth " " MagickWebSite
+#define MagickWebSite  "http://www.imagemagick.org"
+
+/*
   Method declarations.
 */
 extern MagickExport const char
