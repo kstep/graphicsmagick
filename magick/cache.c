0aeb6fc8559491900cc509c9ebfdf2a279b408e7
John Cristy <duc.sequere.aut.de.via.decede@gmail.com>
2002-09-10 21:10:03 -0500

    *** empty log message ***

--- magick/cache.c
+++ magick/cache.c
@@ -251,7 +251,7 @@ MagickExport const PixelPacket *AcquireCacheNexus(const Image *image,
     {
       span=Min(cache_info->columns-(x+u),columns-u);
       if ((((x+u) < 0) || ((x+u) >= (long) cache_info->columns)) ||
-          (((y+v) < 0) || ((y+v) >= (long) cache_info->rows)) || (span == 1))
+          (((y+v) < 0) || ((y+v) >= (long) cache_info->rows)))
         {
           /*
             Transfer a single pixel.
