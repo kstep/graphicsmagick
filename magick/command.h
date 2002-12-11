7a004c788473c1cb9975e202da2150b4fbfe0f52
Glenn Randers-Pehrson <glennrp@comcast.net>
2002-12-11 15:59:40 -0600

    Export utility Usage() "funcons"

--- magick/command.h
+++ magick/command.h
@@ -19,6 +19,17 @@ extern MagickExport unsigned int
   MogrifyImageCommand(ImageInfo *,const int,char **,char **,ExceptionInfo *),
   MontageImageCommand(ImageInfo *,const int,char **,char **,ExceptionInfo *);
 
+extern MagickExport void
+  AnimateUsage(void),
+  ConjureUsage(void),
+  CompositeUsage(void),
+  ConvertUsage(void),
+  DisplayUsage(void),
+  IdentifyUsage(void),
+  ImportUsage(void),
+  MogrifyUsage(void),
+  MontageUsage(void);
+
 #if defined(__cplusplus) || defined(c_plusplus)
 }
 #endif
