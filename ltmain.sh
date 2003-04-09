a4702ae49960fa77928432d0b3c8d4fa4ef60724
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2003-04-09 16:22:09 -0500

    * ltmain.sh: Update to latest CVS libtool.

--- ltmain.sh
+++ ltmain.sh
@@ -56,7 +56,7 @@ modename="$progname"
 PROGRAM=ltmain.sh
 PACKAGE=libtool
 VERSION=1.4e
-TIMESTAMP=" (1.1217 2003/04/02 16:44:37)"
+TIMESTAMP=" (1.1222 2003/04/09 17:54:06)"
 
 default_mode=
 help="Try \`$progname --help' for more information."
@@ -5051,7 +5051,9 @@ fi\
       # Quote the link command for shipping.
       relink_command="(cd `pwd`; $SHELL $0 --mode=relink $libtool_args @inst_prefix_dir@)"
       relink_command=`$echo "X$relink_command" | $Xsed -e "$sed_quote_subst"`
-
+      if test "$hardcode_automatic" = yes ; then
+        relink_command=
+      fi  
       # Only create the output if not a dry run.
       if test -z "$run"; then
 	for installed in no yes; do
