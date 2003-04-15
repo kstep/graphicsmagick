78a14557144c570a693cef6014837d1df0d9a7e9
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2003-04-14 19:56:45 -0500

    * configure.ac: Define PERLMAINCC to be the C compiler if there are no C++ dependencies, or the C++ compiler if there are C++ dependencies. * PerlMagick/Makefile.PL.in: Use PERLMAINCC to compile and link perlmain.c.  This allows using the C++ compiler to link, which is useful when the build depends on C++ libraries like libfpx. * ltmain.sh: Updated to libtool 1.5 release.

--- ltmain.sh
+++ ltmain.sh
@@ -55,8 +55,8 @@ modename="$progname"
 # Constants.
 PROGRAM=ltmain.sh
 PACKAGE=libtool
-VERSION=1.4e
-TIMESTAMP=" (1.1222 2003/04/09 17:54:06)"
+VERSION=1.5
+TIMESTAMP=" (1.1220.2.1 2003/04/14 22:48:00)"
 
 default_mode=
 help="Try \`$progname --help' for more information."
@@ -5051,9 +5051,7 @@ fi\
       # Quote the link command for shipping.
       relink_command="(cd `pwd`; $SHELL $0 --mode=relink $libtool_args @inst_prefix_dir@)"
       relink_command=`$echo "X$relink_command" | $Xsed -e "$sed_quote_subst"`
-      if test "$hardcode_automatic" = yes ; then
-        relink_command=
-      fi  
+
       # Only create the output if not a dry run.
       if test -z "$run"; then
 	for installed in no yes; do
