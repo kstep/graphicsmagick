c522c8ff85dadea6f112adc35de363c98cfb8146
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2003-04-22 19:45:12 -0500

    Prepare for Beta1 release

---
+++ VisualMagick/tests/run_constitute.bat
@@ -0,0 +1,52 @@
+@echo off
+set exedir=..\bin\
+set srcdir=..\..\tests\
+%exedir%constitute -storagetype char %srcdir%input_truecolor.miff RGB 
+%exedir%constitute -storagetype char %srcdir%input_truecolor.miff rgb 
+%exedir%constitute -storagetype char %srcdir%input_truecolor.miff rgba 
+%exedir%constitute -storagetype char %srcdir%input_truecolor.miff cmy 
+%exedir%constitute -storagetype char %srcdir%input_truecolor.miff cmyk 
+%exedir%constitute -storagetype char %srcdir%input_truecolor.miff bGr 
+%exedir%constitute -storagetype char %srcdir%input_truecolor.miff Gbr 
+%exedir%constitute -storagetype char %srcdir%input_truecolor.miff i 
+%exedir%constitute -storagetype short %srcdir%input_truecolor.miff RGB 
+%exedir%constitute -storagetype short %srcdir%input_truecolor.miff rgb 
+%exedir%constitute -storagetype short %srcdir%input_truecolor.miff rgba 
+%exedir%constitute -storagetype short %srcdir%input_truecolor.miff cmy 
+%exedir%constitute -storagetype short %srcdir%input_truecolor.miff cmyk 
+%exedir%constitute -storagetype short %srcdir%input_truecolor.miff bGr 
+%exedir%constitute -storagetype short %srcdir%input_truecolor.miff Gbr 
+%exedir%constitute -storagetype short %srcdir%input_truecolor.miff i 
+%exedir%constitute -storagetype integer %srcdir%input_truecolor.miff RGB 
+%exedir%constitute -storagetype integer %srcdir%input_truecolor.miff rgb 
+%exedir%constitute -storagetype integer %srcdir%input_truecolor.miff rgba 
+%exedir%constitute -storagetype integer %srcdir%input_truecolor.miff cmy 
+%exedir%constitute -storagetype integer %srcdir%input_truecolor.miff cmyk 
+%exedir%constitute -storagetype integer %srcdir%input_truecolor.miff bGr 
+%exedir%constitute -storagetype integer %srcdir%input_truecolor.miff Gbr 
+%exedir%constitute -storagetype integer %srcdir%input_truecolor.miff i 
+%exedir%constitute -storagetype long %srcdir%input_truecolor.miff RGB 
+%exedir%constitute -storagetype long %srcdir%input_truecolor.miff rgb 
+%exedir%constitute -storagetype long %srcdir%input_truecolor.miff rgba 
+%exedir%constitute -storagetype long %srcdir%input_truecolor.miff cmy 
+%exedir%constitute -storagetype long %srcdir%input_truecolor.miff cmyk 
+%exedir%constitute -storagetype long %srcdir%input_truecolor.miff bGr 
+%exedir%constitute -storagetype long %srcdir%input_truecolor.miff Gbr 
+%exedir%constitute -storagetype long %srcdir%input_truecolor.miff i 
+%exedir%constitute -storagetype float %srcdir%input_truecolor.miff RGB 
+%exedir%constitute -storagetype float %srcdir%input_truecolor.miff rgb 
+%exedir%constitute -storagetype float %srcdir%input_truecolor.miff rgba 
+%exedir%constitute -storagetype float %srcdir%input_truecolor.miff cmy 
+%exedir%constitute -storagetype float %srcdir%input_truecolor.miff cmyk 
+%exedir%constitute -storagetype float %srcdir%input_truecolor.miff bGr 
+%exedir%constitute -storagetype float %srcdir%input_truecolor.miff Gbr 
+%exedir%constitute -storagetype float %srcdir%input_truecolor.miff i 
+%exedir%constitute -storagetype double %srcdir%input_truecolor.miff RGB 
+%exedir%constitute -storagetype double %srcdir%input_truecolor.miff rgb 
+%exedir%constitute -storagetype double %srcdir%input_truecolor.miff rgba 
+%exedir%constitute -storagetype double %srcdir%input_truecolor.miff cmy 
+%exedir%constitute -storagetype double %srcdir%input_truecolor.miff cmyk 
+%exedir%constitute -storagetype double %srcdir%input_truecolor.miff bGr 
+%exedir%constitute -storagetype double %srcdir%input_truecolor.miff Gbr 
+%exedir%constitute -storagetype double %srcdir%input_truecolor.miff i 
+
