2271e4b53ccfd32788f9f9801ce4a5e7b60ce23a
Bill Radcliffe <bill.radcliffe@gmail.com>
2003-08-06 12:27:20 -0500

    Fixed major problems with ifdef in first iteration of this code.

--- magick/spinlock.h
+++ magick/spinlock.h
@@ -1,9 +1,8 @@
-#if defined(HasPTHREADS)
-static pthread_mutex_t
-  spinlock_mutex = PTHREAD_MUTEX_INITIALIZER;
-#endif
+#ifndef _SPINLOCKS_H
+#define _SPINLOCKS_H
 
 #if defined(WIN32)
+
 #define SPINLOCK_DELAY_MILLI_SECS 10
 
 static int
@@ -23,16 +22,24 @@ static void _spinlock_release (int *sl)
 {
   InterlockedExchange (sl, 0);
 }
-#if defined(HasPTHREADS)
-#define SPINLOCK_WAIT (void) pthread_mutex_lock(&spinlock_mutex)
-#endif
-#if defined(WIN32)
 #define SPINLOCK_WAIT _spinlock_wait(&spinlock_mutex)
-#endif
-#if defined(HasPTHREADS)
-#define SPINLOCK_RELEASE (void) pthread_mutex_unlock(&spinlock_mutex)
-#endif
-#if defined(WIN32)
 #define SPINLOCK_RELEASE _spinlock_release(&spinlock_mutex)
-#endif
-#endif
+
+#else /* not WIN32 */
+
+#if defined(HasPTHREADS)
+
+static pthread_mutex_t
+  spinlock_mutex = PTHREAD_MUTEX_INITIALIZER;
+#define SPINLOCK_WAIT (void) pthread_mutex_lock(&spinlock_mutex)
+#define SPINLOCK_RELEASE (void) pthread_mutex_unlock(&spinlock_mutex)
+
+#else /* not HasPTHREADS */
+
+#define SPINLOCK_WAIT
+#define SPINLOCK_RELEASE
+
+#endif /* HasPTHREADS */
+
+#endif /* WIN32 */
+#endif /* _SPINLOCKS_H */
