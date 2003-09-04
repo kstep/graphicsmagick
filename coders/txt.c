f4af2dd1cd5386999d2c4e0ade68c0a90b2c432d
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2003-09-04 13:47:19 -0500

    * coders/txt.c (ReadTXTImage): Fix strlen() pointer type warning.

--- coders/txt.c
+++ coders/txt.c
@@ -287,7 +287,7 @@ static Image *ReadTXTImage(const ImageInfo *image_info,ExceptionInfo *exception)
     *draw_info->text='\0';
   p=ReadBlobString(image,text);
 
-  if (IsTXT(p,strlen(p)))
+  if (IsTXT(p,strlen((char *)p)))
     {
       ThrowReaderException(CoderError,NotASupportedImageFile,image);
     }
