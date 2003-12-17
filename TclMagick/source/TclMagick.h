1c92365107772d0e47c8aec3910628b6717c0cc8
David N. Welton <davidw@3@users.sourceforge.net>
2003-12-17 14:59:17 -0600

    * source/TclMagick.h: Updated to latest working TclMagick.h.
    * source/TkMagick.c: Added TkMagick.c, a bridge between TclMagick and
      Tk.
    
    * source/TclMagick.c: Updated to latest file from work done by Rolf
      and myself.
    
    * LICENSE: Added BSD style license with Rolf's name in it.
    
    * README: Added README file.
    
    * ChangeLog: started adding TclMagick code to GraphicsMagick CVS.

--- TclMagick/source/TclMagick.h
+++ TclMagick/source/TclMagick.h
@@ -1,12 +1,39 @@
 /*
+$Id$
  */
 
-#ifndef _EDTLIB_H_
-#define _EDTLIB_H_
+#ifndef _TCLMAGICK_H_
+#define _TCLMAGICK_H_
 
 #include <tcl.h>
+#include <wand/magick_wand.h>
 
 #define DEBUG 1
+
+static CONST char *objTypeNames[] = {
+    "wand", "drawing", "pixel", (char *) NULL
+};
+enum objTypes {
+    TM_TYPE_WAND, TM_TYPE_DRAWING, TM_TYPE_PIXEL, TM_TYPE_ANY
+};
+
+typedef struct {
+    int             type;
+    void            *wandPtr;  /* MagickWand, DrawingWand or PixelWand
+				* pointer */
+    Tcl_Command     magickCmd; /* Token for magick command, used to
+				* delete it */
+    Tcl_Interp      *interp;   /* Tcl interpreter owing the object */
+    Tcl_HashEntry   *hashPtr;  /* Hash entry for this structure, used
+				* to delete it */
+} TclMagickObj;
+
+char *getMagickObjName(TclMagickObj *mPtr);
+TclMagickObj *newMagickObj(Tcl_Interp  *interp, int type,
+			   void *wandPtr, char *name);
+TclMagickObj *findMagickObj(Tcl_Interp *interp, int type, char *name);
+int myMagickError(Tcl_Interp  *interp, MagickWand *wandPtr );
+
 
 #ifdef __WIN32__
 #   define WIN32_LEAN_AND_MEAN
