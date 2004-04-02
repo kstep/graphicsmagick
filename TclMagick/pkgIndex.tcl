3b2ecf0c80321f1e2b50c48c71486c54a55a919e
David N. Welton <davidw@3@users.sourceforge.net>
2004-04-02 06:17:44 -0600

    * pkgIndex.tcl: Fixed pkgIndex.tcl, thanks to Arjen Markus.
    * images/tclmagick*.png: Added a couple of "logos", that will have to
      suffice untill someone artistic has a go at it.

--- TclMagick/pkgIndex.tcl
+++ TclMagick/pkgIndex.tcl
@@ -1,7 +1,7 @@
 # Hand-written package index file.
 
-package ifneeded TclMagick $provides \
+package ifneeded TclMagick 0.40 \
     [list load [file join $dir .. libTclMagick[info sharedlibextension]]]
 
-package ifneeded TkMagick $provides \
+package ifneeded TkMagick 0.40 \
     [list load [file join $dir .. libTkMagick[info sharedlibextension]]]
