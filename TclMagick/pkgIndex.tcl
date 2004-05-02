05a8a91b4f6501a96ac9b6572fa8980042fdf54d
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2004-05-02 18:56:05 -0500

    fixup commit for tag 'GraphicsMagick-1_1_1'

--- TclMagick/pkgIndex.tcl
+++ TclMagick/pkgIndex.tcl
@@ -1,7 +1,7 @@
 # Hand-written package index file.
 
-package ifneeded TclMagick 0.40 \
+package ifneeded TclMagick $provides \
     [list load [file join $dir .. libTclMagick[info sharedlibextension]]]
 
-package ifneeded TkMagick 0.40 \
+package ifneeded TkMagick $provides \
     [list load [file join $dir .. libTkMagick[info sharedlibextension]]]
