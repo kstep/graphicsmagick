4036c91460b1baf7a0cc6444b94d8473027209ac
David N. Welton <davidw@3@users.sourceforge.net>
2004-05-20 16:34:06 -0500

    * Makefile.am (EXTRA_DIST): Add license file, tests and images dirs to   distribution file.
    * configure.ac: Bumped version number to 0.41 because of auto*
      changes.

--- TclMagick/pkgIndex.tcl
+++ TclMagick/pkgIndex.tcl
@@ -1,7 +1,7 @@
 # Hand-written package index file.
 
-package ifneeded TclMagick 0.40 \
+package ifneeded TclMagick 0.41 \
     [list load [file join $dir .. libTclMagick[info sharedlibextension]]]
 
-package ifneeded TkMagick 0.40 \
+package ifneeded TkMagick 0.41 \
     [list load [file join $dir .. libTkMagick[info sharedlibextension]]]
