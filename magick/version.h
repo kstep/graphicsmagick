6be30d237218eedaae8130f5c86d761054ae1a79
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2004-07-09 19:31:32 -0500

    * NEWS: Updated to reflect changes since last update.

--- magick/version.h
+++ magick/version.h
@@ -38,7 +38,7 @@ extern "C" {
 #define MagickLibVersion  0x020000
 #define MagickLibVersionText  "1.2"
 #define MagickLibVersionNumber 2,0,0
-#define MagickChangeDate   "20040708"
+#define MagickChangeDate   "20040709"
 #define MagickReleaseDate  "unreleased"
 
 
