cb35adad16901e040c4bedc470d5d0a6d933d88f
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2004-07-28 20:04:30 -0500

    * Makefile.am:  Update to Automake 1.9.

--- magick/version.h
+++ magick/version.h
@@ -38,7 +38,7 @@ extern "C" {
 #define MagickLibVersion  0x020000
 #define MagickLibVersionText  "1.2"
 #define MagickLibVersionNumber 2,0,0
-#define MagickChangeDate   "20040724"
+#define MagickChangeDate   "20040720"
 #define MagickReleaseDate  "unreleased"
 
 
