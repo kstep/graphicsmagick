115f355b56fb178193bdf197b224e04b07900b3e
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2004-08-07 14:41:09 -0500

    * magick/widget.c (XCommandWidget): Replace ImageMagick logo in display command menu with GraphicsMagick logo.

--- magick/version.h
+++ magick/version.h
@@ -38,7 +38,7 @@ extern "C" {
 #define MagickLibVersion  0x020000
 #define MagickLibVersionText  "1.2"
 #define MagickLibVersionNumber 2,0,0
-#define MagickChangeDate   "20040801"
+#define MagickChangeDate   "20040805"
 #define MagickReleaseDate  "unreleased"
 
 
