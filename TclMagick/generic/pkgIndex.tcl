8e01c1b4cc6d5c22205a6d8d84b2fdd0afc8c298
schroedt <unknown>
2004-08-30 03:20:29 -0500

    pkgIndex.tcl file broken: missing version number in [package ifneeded ...]

--- TclMagick/generic/pkgIndex.tcl
+++ TclMagick/generic/pkgIndex.tcl
@@ -1,1 +1,1 @@
-package ifneeded TclMagick [list load [file join $dir TclMagick[info sharedlibextension]]]
+package ifneeded TclMagick 0.41 [list load [file join $dir TclMagick[info sharedlibextension]]]
