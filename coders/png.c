401a6a42d152a4dad137bf6a6ff265629307152c
Glenn Randers-Pehrson <glennrp@comcast.net>
2005-03-22 14:13:56 -0600

    Fix some little typos in ChangeLog; update copyright date in png.c

--- coders/png.c
+++ coders/png.c
@@ -1,5 +1,5 @@
 /*
-% Copyright (C) 2003, 2004 GraphicsMagick Group
+% Copyright (C) 2003-2005 GraphicsMagick Group
 % Copyright (C) 2002 ImageMagick Studio
 % Copyright 1991-1999 E. I. du Pont de Nemours and Company
 %
