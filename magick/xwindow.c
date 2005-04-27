f9247de36f1ded9574b168b3f84824f4f462a7de
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2005-04-27 10:25:39 -0500

    * NEWS: Added latest news.
    * magick/xwindow.c (XDelay): Prefer use of select() over poll()
    since it is more portable.  MacOS-X has a poll() but it doesn't
    work right.

--- magick/xwindow.c
+++ magick/xwindow.c
@@ -1762,8 +1762,6 @@ MagickExport void XDelay(Display *display,const unsigned long milliseconds)
     timer=milliseconds/1000.0;
     lib$wait(&timer);
   }
-#elif defined(HAVE_POLL)
-  (void) poll((struct pollfd *) NULL,0,(int) milliseconds);
 #elif defined(HAVE_SELECT)
   {
     struct timeval
@@ -1773,6 +1771,8 @@ MagickExport void XDelay(Display *display,const unsigned long milliseconds)
     timer.tv_usec=(long) (milliseconds % 1000)*1000;
     (void) select(0,(XFD_SET *) NULL,(XFD_SET *) NULL,(XFD_SET *) NULL,&timer);
   }
+#elif defined(HAVE_POLL)
+  (void) poll((struct pollfd *) NULL,0,(int) milliseconds);
 #else
 # error "Time delay method not defined."
 #endif
