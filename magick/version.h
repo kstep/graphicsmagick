4b57d9848e25f9117939a31d544eb8cb629a42b9
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2005-12-26 14:31:28 -0600

    * NEWS: Updated with latest changes. * tiff: Update to libtiff 3.7.5.

--- magick/version.h
+++ magick/version.h
@@ -38,7 +38,7 @@ extern "C" {
 #define MagickLibVersion  0x020000
 #define MagickLibVersionText  "1.2"
 #define MagickLibVersionNumber 2,0,0
-#define MagickChangeDate   "20051224"
+#define MagickChangeDate   "20051226"
 #define MagickReleaseDate  "unreleased"
 
 
