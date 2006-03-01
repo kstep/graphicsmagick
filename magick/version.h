a21cdea3354ce1b2f04e8ddaa5954f9fa5ef03f2
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2006-03-01 11:20:33 -0600

    * doc/environment.imdoc: MAGICK_FONT_PATH has not been supported since 1.1.  Remove mention of it!

--- magick/version.h
+++ magick/version.h
@@ -38,7 +38,7 @@ extern "C" {
 #define MagickLibVersion  0x020000
 #define MagickLibVersionText  "1.2"
 #define MagickLibVersionNumber 2,0,0
-#define MagickChangeDate   "20060206"
+#define MagickChangeDate   "20060227"
 #define MagickReleaseDate  "unreleased"
 
 
