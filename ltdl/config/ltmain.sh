ec8115874184d8b6d460fb77f6716e7c59753570
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2006-10-24 19:14:10 -0500

    * Updated libtool to latest CVS head version (1.2352 2006/10/24) in order to fix a bootstrap nit.

--- ltdl/config/ltmain.sh
+++ ltdl/config/ltmain.sh
@@ -1,6 +1,6 @@
 # Generated from ltmain.m4sh; do not edit by hand
 
-# ltmain.sh (GNU libtool 1.2348 2006/10/22 14:10:33) 2.1a
+# ltmain.sh (GNU libtool 1.2352 2006/10/24 20:30:05) 2.1a
 # Written by Gordon Matzigkeit <gord@gnu.ai.mit.edu>, 1996
 
 # Copyright (C) 1996, 1997, 1998, 1999, 2000, 2001, 2003, 2004, 2005, 2006
@@ -64,7 +64,7 @@
 #       compiler:		$LTCC
 #       compiler flags:		$LTCFLAGS
 #       linker:		$LD (gnu? $with_gnu_ld)
-#       $progname:		(GNU libtool 1.2348 2006/10/22 14:10:33) 2.1a
+#       $progname:		(GNU libtool 1.2352 2006/10/24 20:30:05) 2.1a
 #       automake:		$automake_version
 #       autoconf:		$autoconf_version
 #
@@ -73,8 +73,8 @@
 PROGRAM=ltmain.sh
 PACKAGE=libtool
 VERSION=2.1a
-TIMESTAMP=" 1.2348 2006/10/22 14:10:33"
-package_revision=1.2348
+TIMESTAMP=" 1.2352 2006/10/24 20:30:05"
+package_revision=1.2352
 
 # Be Bourne compatible
 if test -n "${ZSH_VERSION+set}" && (emulate sh) >/dev/null 2>&1; then
