daaef1e95aeb6478019e1391dd2cecaa34cd85bb
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2006-11-02 22:42:21 -0600

    * www/Magick++/Image.html: Fix documentation regarding retrieving EXIF attribute. * magick/command.c: Fix typo in usage messages.

--- magick/version.h
+++ magick/version.h
@@ -38,7 +38,7 @@ extern "C" {
 #define MagickLibVersion  0x020000
 #define MagickLibVersionText  "1.2"
 #define MagickLibVersionNumber 2,0,0
-#define MagickChangeDate   "20061024"
+#define MagickChangeDate   "20061102"
 #define MagickReleaseDate  "unreleased"
 
 
