bd9ed349b65922db7c903f16424845b1ff1040ec
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2008-06-05 16:15:07 -0500

    * version.sh: GraphicsMagick 1.2.3 released.

--- magick/version.h
+++ magick/version.h
@@ -35,11 +35,11 @@ extern "C" {
 */
 #define MagickPackageName "GraphicsMagick"
 #define MagickCopyright  "Copyright (C) 2002-2008 GraphicsMagick Group.\nAdditional copyrights and licenses apply to this software.\nSee http://www.GraphicsMagick.org/www/Copyright.html for details."
-#define MagickLibVersion  0x020002
-#define MagickLibVersionText  "1.2.2"
-#define MagickLibVersionNumber 2,0,2
-#define MagickChangeDate   "20080603"
-#define MagickReleaseDate  "2008-05-28"
+#define MagickLibVersion  0x020003
+#define MagickLibVersionText  "1.2.3"
+#define MagickLibVersionNumber 2,0,3
+#define MagickChangeDate   "20080605"
+#define MagickReleaseDate  "2008-06-05"
 
 
 #if (QuantumDepth == 8)
