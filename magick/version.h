a545352e07636a3fabfeed7dae24c76629128924
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2008-06-29 15:29:51 -0500

    * version.sh: GraphicsMagick 1.2.4 released.

--- magick/version.h
+++ magick/version.h
@@ -35,11 +35,11 @@ extern "C" {
 */
 #define MagickPackageName "GraphicsMagick"
 #define MagickCopyright  "Copyright (C) 2002-2008 GraphicsMagick Group.\nAdditional copyrights and licenses apply to this software.\nSee http://www.GraphicsMagick.org/www/Copyright.html for details."
-#define MagickLibVersion  0x020003
-#define MagickLibVersionText  "1.2.3"
-#define MagickLibVersionNumber 2,0,3
+#define MagickLibVersion  0x020004
+#define MagickLibVersionText  "1.2.4"
+#define MagickLibVersionNumber 2,0,4
 #define MagickChangeDate   "20080629"
-#define MagickReleaseDate  "2008-06-05"
+#define MagickReleaseDate  "2008-06-29"
 
 
 #if (QuantumDepth == 8)
--- version.sh
+++ version.sh
@@ -10,12 +10,12 @@ PACKAGE_NAME='GraphicsMagick'
 #
 # Package base version.  This is is the numeric version suffix applied to
 # PACKAGE_NAME (e.g. "1.2").
-PACKAGE_VERSION='1.2.3'
+PACKAGE_VERSION='1.2.4'
 
 #
 # Formal Package release date
 # Set to string "unreleased" if package is not a formal release.
-PACKAGE_RELEASE_DATE="2008-06-05"
+PACKAGE_RELEASE_DATE="2008-06-29"
 #PACKAGE_RELEASE_DATE="unreleased"
 
 #
@@ -64,7 +64,7 @@ CVS_BRANCH_TAG=HEAD
 # Magick library versioning
 #
 MAGICK_LIBRARY_CURRENT=2
-MAGICK_LIBRARY_REVISION=3
+MAGICK_LIBRARY_REVISION=4
 MAGICK_LIBRARY_AGE=0
 
 #
@@ -78,5 +78,5 @@ MAGICK_PLUS_PLUS_LIBRARY_AGE=0
 # Magick Wand library versioning
 #
 MAGICK_WAND_LIBRARY_CURRENT=1
-MAGICK_WAND_LIBRARY_REVISION=0
+MAGICK_WAND_LIBRARY_REVISION=1
 MAGICK_WAND_LIBRARY_AGE=0
