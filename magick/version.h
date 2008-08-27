d9df83a8797c02d04a4ce8258c3530280fc5f022
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2008-08-27 13:48:50 -0500

    * magick/resource.c (ListMagickResourceInfo): Include quantum depth, bits per pixel, and process address size in resource output so that output is more complete.

--- magick/version.h
+++ magick/version.h
@@ -38,7 +38,7 @@ extern "C" {
 #define MagickLibVersion  0x020005
 #define MagickLibVersionText  "1.2.5"
 #define MagickLibVersionNumber 2,0,5
-#define MagickChangeDate   "20080725"
+#define MagickChangeDate   "20080827"
 #define MagickReleaseDate  "2008-07-25"
 
 
