63a44750efacf188e814350b74a23a15f0596086
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2008-11-11 10:25:48 -0600

    * GraphicsMagick.spec.in: Apply RPM spec file fixes from Giacomo Tenaglia for Red Hat Linux 4.

--- magick/version.h
+++ magick/version.h
@@ -38,7 +38,7 @@ extern "C" {
 #define MagickLibVersion  0x030000
 #define MagickLibVersionText  "1.3"
 #define MagickLibVersionNumber 3,0,0
-#define MagickChangeDate   "20081110"
+#define MagickChangeDate   "20081111"
 #define MagickReleaseDate  "2008-11-09"
 
 
