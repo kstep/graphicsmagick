ca61d3624590e9c0d156dbba1dd60aee388c01a2
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2009-01-19 18:55:28 -0600

    fixup commit for branch 'GraphicsMagick-1_3'

---
+++ design/README.txt
@@ -0,0 +1,2 @@
+This directory is to contain any design notes regarding the
+implementation of GraphicsMagick.
---
+++ utilities/tests/preview_grayscale.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Copyright (C) 2008 GraphicsMagick Group
+#
+# This program is covered by multiple licenses, which are described in
+# Copyright.txt. You should have received a copy of Copyright.txt with this
+# package; otherwise see http://www.graphicsmagick.org/www/Copyright.html.
+#
+#  Test for 'gm' utility.
+#
+
+set -e # Exit on any error
+. ${srcdir}/utilities/tests/common.sh
+
+PREVIEW=Grayscale
+OUTFILE="Preview${PREVIEW}_out.miff"
+rm -f ${OUTFILE}
+${GM} convert ${CONVERT_FLAGS} ${MODEL_MIFF} -preview ${PREVIEW} "preview:${OUTFILE}"
