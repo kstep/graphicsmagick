63f547a1e51651700eb1dc0feee2ff91452b6794
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2009-01-23 14:20:30 -0600

    * GraphicsMagick.spec.in: The module .la files need to be installed as part of the base install or else the modules will fail to load.

--- magick/version.h
+++ magick/version.h
@@ -38,7 +38,7 @@ extern "C" {
 #define MagickLibVersion  0x020007
 #define MagickLibVersionText  "1.2.7"
 #define MagickLibVersionNumber 2,0,7
-#define MagickChangeDate   "20090122"
+#define MagickChangeDate   "20090123"
 #define MagickReleaseDate  "2009-01-16"
 
 
