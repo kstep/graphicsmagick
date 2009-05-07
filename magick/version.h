a2f0e06e57d3dbe4e3e630a00233973c4a0c3008
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2009-05-07 18:00:06 -0500

    * ttf: Updated FreeType to 2.3.9.

--- magick/version.h
+++ magick/version.h
@@ -38,7 +38,7 @@ extern "C" {
 #define MagickLibVersion  0x040000
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 4,0,0
-#define MagickChangeDate   "20090505"
+#define MagickChangeDate   "20090507"
 #define MagickReleaseDate  "unreleased"
 
 
