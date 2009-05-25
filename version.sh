43856c1978e83c5fc1e661d0394f5f660bbed94d
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2009-05-25 17:00:35 -0500

    * version.sh: Define PACKAGE_STRING.

--- version.sh
+++ version.sh
@@ -11,7 +11,10 @@ PACKAGE_NAME='GraphicsMagick'
 # Package base version.  This is is the numeric version suffix applied to
 # PACKAGE_NAME (e.g. "1.2").
 PACKAGE_VERSION='1.2.7'
-
+#
+# Package name plus version string.
+#
+PACKAGE_STRING="$PACKAGE_NAME $PACKAGE_VERSION"
 #
 # Formal Package release date
 # Set to string "unreleased" if package is not a formal release.
