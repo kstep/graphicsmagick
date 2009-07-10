6530afcccff313ca7340aff39f8594b453944cb0
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2009-07-10 13:28:42 -0500

    * coders/tiff.c (WriteTIFFImage): In some cases, RowsPerStrip needs to be a multiple of 16 for JPEG in TIFF so ensure that RowsPerStrip is a multiple of 16.

--- coders/tiff.c
+++ coders/tiff.c
@@ -3713,10 +3713,14 @@ static MagickPassFail WriteTIFFImage(const ImageInfo *image_info,Image *image)
         {
         case COMPRESSION_JPEG:
           {
-            /*
-              RowsPerStrip must be multiple of 8 for JPEG.
+	    /*
+              RowsPerStrip must be multiple of 16 for JPEG.
+	      
+	      RowsPerStrip is required to be a multiple of 8 times the
+	      largest vertical sampling factor.  If YCbCr subsampling
+	      is 2,2 then RowsPerStrip must be a multiple of 16.
             */
-            rows_per_strip=rows_per_strip+(8-(rows_per_strip % 8));
+	    rows_per_strip=(((rows_per_strip < 16 ? 16 : rows_per_strip)+1)/16)*16;
             (void) TIFFSetField(tiff,TIFFTAG_JPEGQUALITY,image_info->quality);
             if (IsRGBColorspace(image->colorspace))
               (void) TIFFSetField(tiff,TIFFTAG_JPEGCOLORMODE,JPEGCOLORMODE_RGB);
