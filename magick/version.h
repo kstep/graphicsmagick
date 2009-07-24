5d9a96e10a269b9c1ea137d17951968127e5de69
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2009-07-24 17:08:36 -0500

    Update VisualMagick to include HEAD changes.

--- magick/version.h
+++ magick/version.h
@@ -35,9 +35,9 @@ extern "C" {
 */
 #define MagickPackageName "GraphicsMagick"
 #define MagickCopyright  "Copyright (C) 2002-2009 GraphicsMagick Group.\nAdditional copyrights and licenses apply to this software.\nSee http://www.GraphicsMagick.org/www/Copyright.html for details."
-#define MagickLibVersion  0x040000
-#define MagickLibVersionText  "1.4"
-#define MagickLibVersionNumber 4,0,0
+#define MagickLibVersion  0x040100
+#define MagickLibVersionText  "1.3.6"
+#define MagickLibVersionNumber 4,1,0
 #define MagickChangeDate   "20090723"
 #define MagickReleaseDate  "unreleased"
 
