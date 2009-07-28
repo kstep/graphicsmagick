e534c4c805900b7ea819a24b05a6f096801b8afa
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2009-07-27 21:01:27 -0500

    * coders/pcx.c (ReadPCXImage): Detect improper rows, columns, or depth.  Fixes CVE-2008-1097 "Memory corruption in ImageMagick's PCX coder".
    * configure.ac: Update to Autoconf 2.64.

--- magick/version.h
+++ magick/version.h
@@ -38,7 +38,7 @@ extern "C" {
 #define MagickLibVersion  0x020007
 #define MagickLibVersionText  "1.2.7"
 #define MagickLibVersionNumber 2,0,7
-#define MagickChangeDate   "20090710"
+#define MagickChangeDate   "20090727"
 #define MagickReleaseDate  "2009-01-16"
 
 
