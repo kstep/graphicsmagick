ac6c7be59406ecda6dc2e98fe07fea42fb8eebe2
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2009-08-19 10:32:33 -0500

    * coders/meta.c (ReadMETAImage): Fix memory leak of profile blob.

--- magick/version.h
+++ magick/version.h
@@ -38,7 +38,7 @@ extern "C" {
 #define MagickLibVersion  0x040100
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 4,1,0
-#define MagickChangeDate   "20090818"
+#define MagickChangeDate   "20090819"
 #define MagickReleaseDate  "unreleased"
 
 
