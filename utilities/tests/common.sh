47577f987044a17e7c7dbe3dbda22bc08b05394b
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2009-08-18 22:23:49 -0500

    * utilities/tests/icc-transform.sh: Add a sanity-test for applying ICC profiles.
    * magick/profile.c (ProfileImage): Improve OpenMP performance.

--- utilities/tests/common.sh
+++ utilities/tests/common.sh
@@ -5,10 +5,12 @@ SRCDIR=`cd $SRCDIR && pwd`
 TOPSRCDIR=`cd $srcdir && pwd`
 mkdir -p utilities/tests
 cd utilities/tests || exit 1
+BETARGB_PROFILE="${TOPSRCDIR}/utilities/tests/BetaRGB.icc"
 GENERIC_TTF="${TOPSRCDIR}/PerlMagick/demo/Generic.ttf"
 MODEL_MIFF="${TOPSRCDIR}/Magick++/demo/model.miff"
 SMILE_MIFF="${TOPSRCDIR}/Magick++/demo/smile.miff"
 SUNRISE_JPEG="${TOPSRCDIR}/utilities/tests/sunrise.jpg"
+SUNRISE_MIFF="${TOPSRCDIR}/utilities/tests/sunrise.miff"
 #CONVERT_FLAGS='-monitor'
 #COMPOSITE_FLAGS='-monitor'
 #MONTAGE_FLAGS='-monitor'
