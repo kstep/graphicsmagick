b06a2b2a6633725c639e4c50fdccf6cf302443e0
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2009-09-22 22:51:51 -0500

    * tests/Makefile.am: Add CALS tests.  Skip testing deep images for most formats which don't support deep images.

--- tests/rwfile_AVS_truecolor10.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Copyright (C) 2004-2009 GraphicsMagick Group
-. ${srcdir}/tests/common.shi
-${MEMCHECK} ./rwfile -filespec 'out_truecolor10_%d' ${SRCDIR}/input_truecolor10.dpx AVS
---
+++ tests/rwfile_CALS_truecolor.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Copyright (C) 2004-2009-2009 GraphicsMagick Group
+. ${srcdir}/tests/common.shi
+${MEMCHECK} ./rwfile -filespec 'out_truecolor_%d' ${SRCDIR}/input_truecolor.miff CALS
