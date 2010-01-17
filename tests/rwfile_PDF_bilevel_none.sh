07a2f9a12977281c48dcc9732f938a1592b478f1
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2010-01-17 14:48:13 -0600

    fixup commit for branch 'GraphicsMagick-1_3'

---
+++ tests/rwfile_PDF_bilevel_none.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Copyright (C) 2004-2009 GraphicsMagick Group
+. ${srcdir}/tests/common.shi
+${MEMCHECK} ./rwfile  -filespec 'out_bilevel_none_%d' -compress none ${SRCDIR}/input_bilevel.miff PDF
---
+++ tests/rwfile_PDF_gray_zip.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Copyright (C) 2004-2009 GraphicsMagick Group
+. ${srcdir}/tests/common.shi
+${MEMCHECK} ./rwfile  -filespec 'out_gray_zip_%d' -compress zip ${SRCDIR}/input_gray.miff PDF
