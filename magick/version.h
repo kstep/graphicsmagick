d903cde22e2bb218aca503a8a5fb4d7213d7458d
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2010-02-03 10:55:21 -0600

    * magick/display.c (MagickXDisplayImage): The display '-update' option was only working in conjunction with the '-delay' option with a delay setting of 2 or greater.  Problem reported by Sami Liedes.  Patch by Vincent MAUGE.  Resolves Debian bug ID 414779.

--- magick/version.h
+++ magick/version.h
@@ -38,7 +38,7 @@ extern "C" {
 #define MagickLibVersion  0x060300
 #define MagickLibVersionText  "1.3.8"
 #define MagickLibVersionNumber 6,3,0
-#define MagickChangeDate   "20100202"
+#define MagickChangeDate   "20100203"
 #define MagickReleaseDate  "2010-01-21"
 	
 /*
