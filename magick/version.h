f098fe01c9d9261dc011e62fa409dd8954f22e7b
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2011-01-30 14:00:57 -0600

    Update Copyright year.

--- magick/version.h
+++ magick/version.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2003 - 2010 GraphicsMagick Group
+  Copyright (C) 2003 - 2011 GraphicsMagick Group
   Copyright (C) 2002 ImageMagick Studio
   Copyright 1991-1999 E. I. du Pont de Nemours and Company
  
@@ -34,7 +34,7 @@ extern "C" {
   identifying the release.
 */
 #define MagickPackageName "GraphicsMagick"
-#define MagickCopyright  "Copyright (C) 2002-2010 GraphicsMagick Group.\nAdditional copyrights and licenses apply to this software.\nSee http://www.GraphicsMagick.org/www/Copyright.html for details."
+#define MagickCopyright  "Copyright (C) 2002-2011 GraphicsMagick Group.\nAdditional copyrights and licenses apply to this software.\nSee http://www.GraphicsMagick.org/www/Copyright.html for details."
 #define MagickLibVersion  0x090600
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 9,6,0
