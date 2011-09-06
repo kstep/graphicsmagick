adc6fa29fd8b73fe3bf0400c7b14ed315c967f71
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2011-09-06 13:53:15 -0500

    Import lcms2 2.2

--- lcms/Projects/BorlandC_5.5/mklcmsdll.bat
+++ lcms/Projects/BorlandC_5.5/mklcmsdll.bat
@@ -6,7 +6,7 @@ echo Press Ctrl-C to abort, or
 pause
 bcc32 @lcmsdll.lst
 if errorlevel 0 ilink32 @lcmsdll.lk
-if errorlevel 0 brc32 -fe ..\..\bin\lcms.dll lcms.rc
+if errorlevel 0 brc32 -fe ..\..\bin\lcms2.dll lcms2.rc
 del *.obj
 del *.res
 echo Done!
