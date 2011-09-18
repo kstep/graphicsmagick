53c3c767f8a369d1c6fc733fcc15b3c0c4de4b76
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2011-09-18 10:22:35 -0500

    Windows installer now installs .mgk files to top of app directory.

--- magick/version.h
+++ magick/version.h
@@ -38,7 +38,7 @@ extern "C" {
 #define MagickLibVersion  0x090600
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 9,6,0
-#define MagickChangeDate   "20110823"
+#define MagickChangeDate   "20110918"
 #define MagickReleaseDate  "unreleased"
 	
 /*
