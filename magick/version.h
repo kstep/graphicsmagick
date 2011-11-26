1526a91b9d4f61445db58c89a972edec8969e6b8
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2011-11-26 12:17:21 -0600

    File names in gnuplot files need to be surrounded by double quotes.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x090600
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 9,6,0
-#define MagickChangeDate   "20111125"
-#define MagickReleaseDate  "snapshot-20111125"
+#define MagickChangeDate   "20111126"
+#define MagickReleaseDate  "snapshot-20111126"
 	
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
