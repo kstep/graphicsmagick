e728ece01b2cd4498c9eebe3f3c9a7ff47651438
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2011-12-05 21:11:15 -0600

    Eliminate use of deprecated BuildPrereq in RPM spec file

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x090600
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 9,6,0
-#define MagickChangeDate   "20111128"
-#define MagickReleaseDate  "snapshot-20111128"
+#define MagickChangeDate   "20111205"
+#define MagickReleaseDate  "snapshot-20111205"
 	
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
