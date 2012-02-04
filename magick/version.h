b7d223e0580c2c5c8cd7d3a6b01fd5eafa1e027a
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2012-02-04 14:46:46 -0600

    Update to Automake 1.11.3. add lzip compressed archive.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x100700
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 10,7,0
-#define MagickChangeDate   "20120130"
-#define MagickReleaseDate  "snapshot-20120130"
+#define MagickChangeDate   "20120204"
+#define MagickReleaseDate  "snapshot-20120204"
 	
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
