19e7b8a11dc65040ee146a8f550a543a7f0c73c2
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2012-02-09 22:30:12 -0600

    Fix reading XCF which is comprised of differend sized layers.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x100700
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 10,7,0
-#define MagickChangeDate   "20120205"
-#define MagickReleaseDate  "snapshot-20120205"
+#define MagickChangeDate   "20120209"
+#define MagickReleaseDate  "snapshot-20120209"
 	
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
