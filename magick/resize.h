d8a8b18bdc74a032284be53e8d137c8ec34b1997
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2012-03-30 09:54:54 -0500

    Thumbnail defaults to box filter but should allow user to overide filter.

--- magick/resize.h
+++ magick/resize.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2003 GraphicsMagick Group
+  Copyright (C) 2003 - 2012 GraphicsMagick Group
   Copyright (C) 2002 ImageMagick Studio
  
   This program is covered by multiple licenses, which are described in
@@ -16,6 +16,7 @@ extern "C" {
 #endif /* defined(__cplusplus) || defined(c_plusplus) */
 
 #define DefaultResizeFilter LanczosFilter
+#define DefaultThumbnailFilter BoxFilter
 
 extern MagickExport Image
   *MagnifyImage(const Image *,ExceptionInfo *),
