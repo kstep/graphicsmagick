95131aefef9c23230110a623a4c14d8bb169bdd7
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2012-05-15 09:03:22 -0500

    Provide support for size_t and ssize_t in magick/magick_config_api.h.in

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x110800
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 11,8,0
-#define MagickChangeDate   "20120514"
-#define MagickReleaseDate  "snapshot-20120514"
+#define MagickChangeDate   "20120515"
+#define MagickReleaseDate  "snapshot-20120515"
 	
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
