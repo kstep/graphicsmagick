de4e35b0b1d7afbe1660767f87321f3d590c1e5e
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2012-09-15 09:26:02 -0500

    Fix Fix Debian bug 687738 "graphicsmagick: repeated words suitable for suitable for in gm manpage"

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x120900
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 12,9,0
-#define MagickChangeDate   "20120908"
-#define MagickReleaseDate  "snapshot-20120908"
+#define MagickChangeDate   "20120915"
+#define MagickReleaseDate  "snapshot-20120915"
 	
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
