2998917a51362c0454428314cda1e66b593b9205
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2012-10-13 18:03:04 -0500

    Remove files mysteriously not removed by merge

--- tests/constitute_char_bgrp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Copyright (C) 2004-2010 GraphicsMagick Group
-. ${srcdir}/tests/common.shi
-${MEMCHECK} ./constitute -storagetype char ${SRCDIR}/input_truecolor.miff bgrp
--- tests/constitute_float_rgbo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Copyright (C) 2004-2010 GraphicsMagick Group
-. ${srcdir}/tests/common.shi
-${MEMCHECK} ./constitute -storagetype float ${SRCDIR}/input_truecolor.miff rgbo
