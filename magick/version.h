16185eaeffad5d3d8bc67e6c8f17815a177d165b
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2012-11-23 16:03:02 -0600

    Add a simple example of using the Wand API.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x131000
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 13,10,0
-#define MagickChangeDate   "20121121"
-#define MagickReleaseDate  "snapshot-20121121"
+#define MagickChangeDate   "20121123"
+#define MagickReleaseDate  "snapshot-20121123"
 	
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
