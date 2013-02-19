3614dbd3c5eb81c90d5872d49e3b577f66ba563d
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2013-02-18 18:57:57 -0600

    Restore local libtool patch.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x131000
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 13,10,0
-#define MagickChangeDate   "20130213"
-#define MagickReleaseDate  "snapshot-20130213"
+#define MagickChangeDate   "20130218"
+#define MagickReleaseDate  "snapshot-20130218"
 	
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
