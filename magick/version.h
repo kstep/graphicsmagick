31642fd6bc59b80e358e28ba1987a0b0de38b11d
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2013-03-17 14:39:27 -0500

    INSTALL-unix.txt: Add a section about building Windows binaries by cross-compiling from a Unix/Linux system.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x141100
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 14,11,0
-#define MagickChangeDate   "20130316"
-#define MagickReleaseDate  "snapshot-20130316"
+#define MagickChangeDate   "20130317"
+#define MagickReleaseDate  "snapshot-20130317"
 	
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
