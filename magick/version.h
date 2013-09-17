49ddf345ea6d82e644e60951fe849a434c4a4630
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2013-09-16 19:45:20 -0500

    Fix assertion/crash noticed when lcms returned a NULL CMS transform.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x141100
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 14,11,0
-#define MagickChangeDate   "20130908"
-#define MagickReleaseDate  "snapshot-20130908"
+#define MagickChangeDate   "20130916"
+#define MagickReleaseDate  "snapshot-20130916"
 	
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
