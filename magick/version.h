5669c79662a17a9eb71d749889b652d3c547e771
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2014-01-03 20:12:04 -0600

    Fix PerlMagick input_16.png signatures.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x141101
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 14,11,1
-#define MagickChangeDate   "20140101"
-#define MagickReleaseDate  "snapshot-20140101"
+#define MagickChangeDate   "20140102"
+#define MagickReleaseDate  "snapshot-20140102"
 	
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
