32ffecbc3e29adb7a31e08b72aa921d599169e7f
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2014-01-21 20:31:16 -0600

    PAM fix for scaling of alpha in sub-ranged pixels.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x141101
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 14,11,1
-#define MagickChangeDate   "20140119"
-#define MagickReleaseDate  "snapshot-20140119"
+#define MagickChangeDate   "20140121"
+#define MagickReleaseDate  "snapshot-20140121"
 	
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
