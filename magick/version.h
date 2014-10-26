590befc95f64c300aaf8ac69ef481a6f3d3717c3
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2014-10-26 11:11:21 -0500

    PCX fixes: Additional header sanity check, fix memory leaks, add header logging.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x151200
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 15,12,0
-#define MagickChangeDate   "20141014"
-#define MagickReleaseDate  "snapshot-20141014"
+#define MagickChangeDate   "20141026"
+#define MagickReleaseDate  "snapshot-20141026"
 	
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
