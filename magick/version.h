31e018cae9ece5c6b46270368a67052ee6309ea8
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2015-01-24 14:41:07 -0600

    FITS: Enforce that BITPIX bit-per-pixel values are valid.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x151200
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 15,12,0
-#define MagickChangeDate   "20150122"
-#define MagickReleaseDate  "snapshot-20150122"
+#define MagickChangeDate   "20150124"
+#define MagickReleaseDate  "snapshot-20150124"
 	
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
