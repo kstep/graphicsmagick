d28c13d715fcc247250f6f6d7c1a55841cc84e2f
Bob Friesenhahn <bfriesen@GraphicsMagick.org>
2015-03-10 11:10:37 -0500

    Document JPEG-specific -format tags.

--- magick/version.h
+++ magick/version.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MagickLibVersion  0x161300
 #define MagickLibVersionText  "1.4"
 #define MagickLibVersionNumber 16,13,0
-#define MagickChangeDate   "20150307"
-#define MagickReleaseDate  "snapshot-20150307"
+#define MagickChangeDate   "20150310"
+#define MagickReleaseDate  "snapshot-20150310"
 	
 /*
   The MagickLibInterfaceNewest and MagickLibInterfaceOldest defines
